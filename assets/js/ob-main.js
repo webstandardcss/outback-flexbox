a74b51c8b970cedf589fafd1a78e9a620ba9d9c6
Trey Brister <webstandardcss@gmail.com>
2018-03-25 00:59:27 -0500

    First Commit

---
+++ assets/js/ob-main.js
@@ -0,0 +1,54 @@
+// Outback Javascript
+$(document).ready(function () {
+  // Main navigation menu
+  var dropdownLink = $(".sf-dropdown");
+  var dropdownMenu = $(".sf-dropdown-menu");
+  // Hide the menu on page load
+  dropdownMenu.hide();
+  // When clicking menu link, show the nested sub menu.
+  dropdownLink.click(function () {
+    dropdownMenu.slideUp('medium');
+    $(this).find(dropdownMenu).slideToggle('medium');
+  });
+
+  // This below commented code may not work.
+  // It is for if we ever try a third level menu.
+  // var subMenu = $('li.sf-dropdown > ul > li');
+  // subMenu.click(function () {
+  //   $(".sf-dropdown-menu").hide();
+  //   $(this).find("ul").slideToggle(200);
+  //   var href = $(this).attr('href');
+  //   // Delay setting the location
+  //   setTimeout(function() {window.location = href}, 5000);
+  //   return false;
+  // });
+});
+
+$(document).ready(function () {
+  // Mobile navigation
+  var dropdownSelect = $(".navselect");
+  var dropdownButton = $(".mobilenav-button");
+  var dropdownLength = $(".navselect > option").length;
+  var pageBody = $(".ob-main");
+  // Hide the menu on page load
+  dropdownSelect.hide();
+  // When clicking menu button, hide the button and show the menu.
+  dropdownButton.click(function (e) {
+   dropdownSelect.toggle().focus().attr('size', dropdownLength);
+  });
+  // When clicking menu, hide the clicked menu and show the button.
+  dropdownSelect.click(function () {
+    dropdownSelect.hide();
+    dropdownButton.show();
+  });
+  // Follow link from the selected option value
+  dropdownSelect.change(function () {
+    window.location = $(this).find("option:selected").val();
+  });
+});
+
+// TODO Create PHP Pages
+// TODO Create Content Pages
+// TODO Implement Tipue Search or Google API search
+// http://www.tipue.com/beaty/
+// http://www.tipue.com/search/
---
+++ inc/footer.php
@@ -0,0 +1,28 @@
+<footer id="containerfooter" class="content-info " role="contentinfo" style="background: #008191;  padding:0; height: auto;">
+  <div class="container">
+    <div class="row">
+    </div>
+    <!--<div class="footerclass"></div>-->
+    <div id="content" class="container head-bottom">
+      <div class="row">
+        <div class="left-side"><img src="http://outbackfeeders.com/wp-content/uploads/2015/06/American-Flag1.png"></div>
+        <div class="right-side call"><a href="tel:+18003966313">800.396.6313</a></div>
+      </div>
+    </div>
+  </div>
+  <div class="copyright">
+    <div class="container">
+      <div class="row">
+        <p></p>
+        <p class="line"><font size="5">Outback Wildlife Feeders &copy; <b>800-396-6313</b></font><font size="3"> - Web Design by <a href="http://www.rkvideo.tv" target="_blank"> RK Productions</a></font></p>
+      </div>
+      <div class="social-icn">
+        <img class="alignright  wp-image-757" alt="creditcard icons_MC" src="http://outbackfeeders.com/wp-content/uploads/2015/02/creditcard-icons_MC.png" width="70" height="45">
+        <img class="alignright  wp-image-758" alt="creditcard icons_AE" src="http://outbackfeeders.com/wp-content/uploads/2015/02/creditcard-icons_AE.png" width="70" height="45"> <img class="alignright  wp-image-756" alt="creditcard icons_visa" src="http://outbackfeeders.com/wp-content/uploads/2015/02/creditcard-icons_visa.png" width="70" height="45">
+        <a href="https://plus.google.com/105053570710154312351" rel="publisher"><img src="http://outbackfeeders.com/wp-content/uploads/2015/06/g+1.png" alt="g+" class="alignright size-full wp-image-969" height="55" width="55" style="float: right;margin: 0 30px 0 0;"></a>
+        <a href="http://outbackfeeders.com/wp-content/uploads/2014/01/Facebook_logo2.png"><img src="http://outbackfeeders.com/wp-content/uploads/2015/06/fb1.png" alt="fb" class="alignright size-full wp-image-970" height="55" width="55"></a>
+      </div>
+      <p></p>
+    </div>
+  </div>
+</footer>

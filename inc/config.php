d395a73ab072ebab303bf11ec8b69a7a71ccff8f
Trey Brister <webstandardcss@gmail.com>
2018-03-25 23:31:42 -0500

    PHP website configuration data

---
+++ inc/config.php
@@ -0,0 +1,15 @@
+<?php
+$siteName = "Outback Feeders";
+$siteTwitter = "@OutbackFeeders";
+$siteKeywords = ",outback,feeders,hunting,deer feeders";
+$siteFacebookAdmin = "Facebook admin numeric ID";
+$siteFacebookURL = "https://www.facebook.com/Outback-Wildlife-Feeders-and-Furniture-249061648570558/";
+// $siteHost = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://" . $_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI];
+$siteHost = "http://" . $_SERVER[HTTP_HOST];
+$pageURL = $siteHost . $_SERVER[REQUEST_URI];
+$pageImage = $siteHost . "assets/images/logo.png";
+$pageDateModifiedFriendly = date("F d Y H:i:s",filemtime($pageURL));
+// February 14 2006 13:22:46
+$pageDateModified = date("Y-F-d\TH:i:s-5:00",filemtime($pageURL));
+// 2013-09-17T05:59:00+01:00
+?>

5e80579253f1ced2f1d2e95d4c747725016d6699
Trey Brister <webstandardcss@gmail.com>
2018-03-26 12:20:53 -0500

    quote array keys

--- inc/config.php
+++ inc/config.php
@@ -8,9 +8,9 @@ $siteFacebookAdmin = "Facebook admin numeric ID";
 $siteFacebookURL = "https://www.facebook.com/Outback-Wildlife-Feeders-and-Furniture-249061648570558/";
 $siteGoogleURL = "https://plus.google.com/105053570710154312351";
 // $siteHost = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://" . $_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI];
-$siteHost = "http://" . $_SERVER[HTTP_HOST];
-$pageURL = $siteHost . $_SERVER[REQUEST_URI] . '/';
-$pageImage = $siteHost . "assets/images/logo.png";
+$siteHost = "http://" . $_SERVER['HTTP_HOST'];
+$pageURL = $siteHost . $_SERVER['REQUEST_URI'];
+$pageImage = $siteHost . "/assets/images/logo.png";
 $pageDateModifiedFriendly = date("F d Y H:i:s",filemtime($pageURL));
 // February 14 2006 13:22:46
 $pageDateModified = date("Y-F-d\TH:i:s-5:00",filemtime($pageURL));

44c3b878a50b24465b70be62ace567d12ec78f42
Trey Brister <webstandardcss@gmail.com>
2018-03-26 02:28:32 -0500

    setup footer data in central location

--- inc/config.php
+++ inc/config.php
@@ -1,12 +1,15 @@
 <?php
-$siteName = "Outback Feeders";
+$siteName = "Outback Wildlife Feeders";
+$sitePhone = "800-396-6313";
+$sitePhoneRaw = "+18003966313";
 $siteTwitter = "@OutbackFeeders";
 $siteKeywords = ",outback,feeders,hunting,deer feeders";
 $siteFacebookAdmin = "Facebook admin numeric ID";
 $siteFacebookURL = "https://www.facebook.com/Outback-Wildlife-Feeders-and-Furniture-249061648570558/";
+$siteGoogleURL = "https://plus.google.com/105053570710154312351";
 // $siteHost = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://" . $_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI];
 $siteHost = "http://" . $_SERVER[HTTP_HOST];
-$pageURL = $siteHost . $_SERVER[REQUEST_URI];
+$pageURL = $siteHost . $_SERVER[REQUEST_URI] . '/';
 $pageImage = $siteHost . "assets/images/logo.png";
 $pageDateModifiedFriendly = date("F d Y H:i:s",filemtime($pageURL));
 // February 14 2006 13:22:46
--- inc/footer.php
+++ inc/footer.php
@@ -2,14 +2,14 @@
 <footer id="containerfooter" class="footer flex-container" role="contentinfo">
   <div id="content" class="container footer-top flex-container-row container-vertical-center">
       <div class="left-side"><img src="assets/images/American-Flag1.png" class="image-pride-usa"></div>
-      <div class="right-side call"><a href="tel:+18003966313">800.396.6313</a></div>
+      <div class="right-side call"><a href="tel:<?php echo $sitePhoneRaw; ?>"><?php echo $sitePhone; ?></a></div>
   </div>
   <div class="footer-bottom container flex-container-row container-vertical-center">
     <div class="footer-credits left-side">
-      <p>Outback Wildlife Feeders &copy; <b>800-396-6313</b> - <a href="http://www.rkvideo.tv" target="_blank">Web Design by RK Productions</a></p>
+      <p><?php echo $siteName; ?> &copy; <b><?php echo $sitePhone; ?></b> - <a href="http://www.rkvideo.tv" target="_blank">Web Design by RK Productions</a></p>
     </div>
     <div class="social-icons right-side">
-      <a href="https://plus.google.com/105053570710154312351" rel="publisher">
+      <a href="<?php echo $siteGoogleURL; ?>" rel="publisher">
         <img src="assets/images/footer-icons.png" class="footer-icons" id="footer-icons" alt="Find us on Facebook, Google+. We accept Visa, American Express, and MasterCard" />
       </a>
     </div>
@@ -20,7 +20,6 @@
     <a href="#"><i class="fas fa-arrow-circle-up"></i></a>
   </div>
 </div>
-
 <script src="assets/js/ob-main.js"></script>
 <script src="custom.js"></script>
 </body>

4f0b9e529485fabb660dce3408a7007cb96263bd
Trey Brister <webstandardcss@gmail.com>
2018-03-25 22:30:53 -0500

    links to new php included required files in the existing pages

--- inc/_nav.php
+++ inc/_nav.php
@@ -3,7 +3,7 @@
     <li class="menu-home menu-home"><a href="http://outbackfeeders.com/">Home</a></li>
     <li class="menu-about-us sf-dropdown menu-about-us">
       <a href="http://outbackfeeders.com/outback-story-guarantee/" class="sf-with-ul">About Us<span class="sf-sub-indicator"> »</span></a>
-      <ul class="sf-dropdown-menu" style="display: none;">
+      <ul class="sf-dropdown-menu">
         <li class="menu-contact-us menu-contact-us"><a href="http://outbackfeeders.com/contact-us/">Contact Us</a></li>
         <li class="menu-delivery-information menu-delivery-information"><a href="http://outbackfeeders.com/about-us/delivery-information/">Delivery Information</a></li>
         <li class="menu-outback-story-guarantee menu-outback-story-guarantee"><a href="http://outbackfeeders.com/outback-story-guarantee/">Outback Story &amp; Guarantee</a></li>
@@ -12,14 +12,14 @@
     <li class="menu-why-you-should-buy-outback-feeders menu-why-you-should-buy-outback-feeders"><a href="http://outbackfeeders.com/why-you-should-buy-outback-feeders/">Why you should buy Outback Feeders</a></li>
     <li class="menu-corn-feeders sf-dropdown menu-corn-feeders">
       <a href="http://outbackfeeders.com/corn-feeders/" class="sf-with-ul">Corn Feeders<span class="sf-sub-indicator"> »</span></a>
-      <ul class="sf-dropdown-menu" style="display: none;">
+      <ul class="sf-dropdown-menu"">
         <li class="menu-baby-back-low-profile-feeders menu-baby-back-low-profile-feeders"><a href="http://outbackfeeders.com/baby-back-series/">BABY-BACK Low Profile Feeders</a></li>
-        <li class="menu-top-gun-corn-feeder menu-top-gun-corn-feeder"><a href="http://outbackfeeders.com/top-gun-corn-feeder/">Top Gun Corn Feeder</a></li>
+        <li class="menu-top-gun-corn-feeder menu-top-gun-corn-f\eeder"><a href="http://outbackfeeders.com/top-gun-corn-feeder/">Top Gun Corn Feeder</a></li>
       </ul>
     </li>
     <li class="menu-protein-feeders sf-dropdown menu-protein-feeders">
       <a href="http://outbackfeeders.com/protein-feeders/" class="sf-with-ul">Protein Feeders<span class="sf-sub-indicator"> »</span></a>
-      <ul class="sf-dropdown-menu" style="display: none;">
+      <ul class="sf-dropdown-menu">
         <li class="menu-half-back-protein-feeder menu-half-back-protein-feeder"><a href="http://outbackfeeders.com/half-back-protein/">Half-Back Protein Feeder</a></li>
         <li class="menu-the-doc menu-the-doc"><a href="http://outbackfeeders.com/the-doc/">The Doc</a></li>
         <li class="menu-the-coontroller menu-the-coontroller"><a href="http://outbackfeeders.com/the-coontroller/">THE COONTROLLER</a></li>
@@ -31,7 +31,7 @@
     <li class="menu-water-stations menu-water-stations"><a href="http://outbackfeeders.com/water-stations/">Water Stations</a></li>
     <li class="menu-feed-delivery-systems sf-dropdown menu-feed-delivery-systems">
       <a href="http://outbackfeeders.com/feed-delivery-systems/" class="sf-with-ul">Feed Delivery Systems<span class="sf-sub-indicator"> »</span></a>
-      <ul class="sf-dropdown-menu" style="display: none;">
+      <ul class="sf-dropdown-menu">
         <li class="menu-the-road-doc menu-the-road-doc"><a href="http://outbackfeeders.com/new-products/">THE ROAD DOC</a></li>
         <li class="menu-bulk-trailers menu-bulk-trailers"><a href="http://outbackfeeders.com/bulk-feed-systems/bulk-trailers/">Bulk Trailers</a></li>
         <li class="menu-bulk-hoppers menu-bulk-hoppers"><a href="http://outbackfeeders.com/bulk-feed-systems/bulk-hoppers/">Bulk Hoppers</a></li>
@@ -47,41 +47,4 @@
     <li class="menu-outback-dealer-list menu-outback-dealer-list"><a href="http://outbackfeeders.com/about-us/outback-dealers/">Outback Dealer List</a></li>
     <li class="menu-kickin-back-kreations menu-kickin-back-kreations"><a href="http://KickinBackKreations.com">Kickin’ Back Kreations</a></li>
   </ul>
-  <select id="menu-main-1" class="navselect">
-    <option value="http://outbackfeeders.com/">Home</option>
-    <option value="http://outbackfeeders.com/outback-story-guarantee/">About Us
-    </option>
-    <option value="http://outbackfeeders.com/contact-us/">–&nbsp;Contact Us</option>
-    <option value="http://outbackfeeders.com/about-us/delivery-information/">–&nbsp;Delivery Information</option>
-    <option value="http://outbackfeeders.com/outback-story-guarantee/">–&nbsp;Outback Story &amp; Guarantee</option>
-    <option value="http://outbackfeeders.com/why-you-should-buy-outback-feeders/">Why you should buy Outback Feeders</option>
-    <option value="http://outbackfeeders.com/corn-feeders/">Corn Feeders
-    </option>
-    <option value="http://outbackfeeders.com/baby-back-series/">–&nbsp;BABY-BACK Low Profile Feeders</option>
-    <option value="http://outbackfeeders.com/top-gun-corn-feeder/">–&nbsp;Top Gun Corn Feeder</option>
-    <option value="http://outbackfeeders.com/protein-feeders/">Protein Feeders
-    </option>
-    <option value="http://outbackfeeders.com/half-back-protein/">–&nbsp;Half-Back Protein Feeder</option>
-    <option value="http://outbackfeeders.com/the-doc/">–&nbsp;The Doc</option>
-    <option value="http://outbackfeeders.com/the-coontroller/">–&nbsp;THE COONTROLLER</option>
-    <option value="http://outbackfeeders.com/protein-corn-combo-feeders/">Protein &amp; Corn Combo Feeders</option>
-    <option value="http://outbackfeeders.com/fish-feeder-share-kropper/">Fish Feeder</option>
-    <option selected="selected" value="http://outbackfeeders.com/quail-turkey-feeders/">Quail &amp; Turkey Feeders</option>
-    <option value="http://outbackfeeders.com/water-stations/">Water Stations</option>
-    <option value="http://outbackfeeders.com/feed-delivery-systems/">Feed Delivery Systems
-    </option>
-    <option value="http://outbackfeeders.com/new-products/">–&nbsp;THE ROAD DOC</option>
-    <option value="http://outbackfeeders.com/bulk-feed-systems/bulk-trailers/">–&nbsp;Bulk Trailers</option>
-    <option value="http://outbackfeeders.com/bulk-feed-systems/bulk-hoppers/">–&nbsp;Bulk Hoppers</option>
-    <option value="http://outbackfeeders.com/bulk-feed-systems/bulk-bins/">–&nbsp;Bulk Bins</option>
-    <option value="http://outbackfeeders.com/traps-pens/">Traps &amp; Pens</option>
-    <option value="http://outbackfeeders.com/hunt-camp-equipment/">Hunt &amp; Camp Equipment</option>
-    <option value="http://outbackfeeders.com/specialty-products/">Specialty Products</option>
-    <option value="http://outbackfeeders.com/pre-owned-feeders/">Pre Owned Feeders</option>
-    <option value="http://outbackfeeders.com/trophy-gallery/">Trophy Gallery</option>
-    <option value="http://outbackfeeders.com/become-a-dealer/">Become a Dealer</option>
-    <option value="http://outbackfeeders.com/about-us/outback-dealers/">Outback Dealer List</option>
-    <option value="http://KickinBackKreations.com">Kickin' Back Kreations</option>
-  </select>
-  <div class="mobilenav-button"><i class="icon-reorder"></i><span class="headerfont">menu</span></div>
 </nav>
--- inc/footer.php
+++ inc/footer.php
@@ -1,28 +1,27 @@
-<footer id="containerfooter" class="content-info " role="contentinfo" style="background: #008191;  padding:0; height: auto;">
-  <div class="container">
-    <div class="row">
-    </div>
-    <!--<div class="footerclass"></div>-->
-    <div id="content" class="container head-bottom">
-      <div class="row">
-        <div class="left-side"><img src="http://outbackfeeders.com/wp-content/uploads/2015/06/American-Flag1.png"></div>
-        <div class="right-side call"><a href="tel:+18003966313">800.396.6313</a></div>
-      </div>
-    </div>
+<!-- <footer id="containerfooter" class="footer flex-container" role="contentinfo" style="background: #008191;  padding:0; height: auto;"> -->
+<footer id="containerfooter" class="footer flex-container" role="contentinfo">
+  <div id="content" class="container footer-top flex-container-row container-vertical-center">
+      <div class="left-side"><img src="assets/images/American-Flag1.png" class="image-pride-usa"></div>
+      <div class="right-side call"><a href="tel:+18003966313">800.396.6313</a></div>
   </div>
-  <div class="copyright">
-    <div class="container">
-      <div class="row">
-        <p></p>
-        <p class="line"><font size="5">Outback Wildlife Feeders &copy; <b>800-396-6313</b></font><font size="3"> - Web Design by <a href="http://www.rkvideo.tv" target="_blank"> RK Productions</a></font></p>
-      </div>
-      <div class="social-icn">
-        <img class="alignright  wp-image-757" alt="creditcard icons_MC" src="http://outbackfeeders.com/wp-content/uploads/2015/02/creditcard-icons_MC.png" width="70" height="45">
-        <img class="alignright  wp-image-758" alt="creditcard icons_AE" src="http://outbackfeeders.com/wp-content/uploads/2015/02/creditcard-icons_AE.png" width="70" height="45"> <img class="alignright  wp-image-756" alt="creditcard icons_visa" src="http://outbackfeeders.com/wp-content/uploads/2015/02/creditcard-icons_visa.png" width="70" height="45">
-        <a href="https://plus.google.com/105053570710154312351" rel="publisher"><img src="http://outbackfeeders.com/wp-content/uploads/2015/06/g+1.png" alt="g+" class="alignright size-full wp-image-969" height="55" width="55" style="float: right;margin: 0 30px 0 0;"></a>
-        <a href="http://outbackfeeders.com/wp-content/uploads/2014/01/Facebook_logo2.png"><img src="http://outbackfeeders.com/wp-content/uploads/2015/06/fb1.png" alt="fb" class="alignright size-full wp-image-970" height="55" width="55"></a>
-      </div>
-      <p></p>
+  <div class="footer-bottom container flex-container-row container-vertical-center">
+    <div class="footer-credits left-side">
+      <p>Outback Wildlife Feeders &copy; <b>800-396-6313</b> - <a href="http://www.rkvideo.tv" target="_blank">Web Design by RK Productions</a></p>
+    </div>
+    <div class="social-icons right-side">
+      <a href="https://plus.google.com/105053570710154312351" rel="publisher">
+        <img src="assets/images/footer-icons.png" class="footer-icons" id="footer-icons" alt="Find us on Facebook, Google+. We accept Visa, American Express, and MasterCard" />
+      </a>
     </div>
   </div>
 </footer>
+<div id="topcontrol" title="To the top!" style="position: fixed; bottom: 52px; right: 0px; opacity: 1; cursor: pointer;">
+  <div class="to-the-top">
+    <a href="#"><i class="fas fa-arrow-circle-up"></i></a>
+  </div>
+</div>
+
+<script src="assets/js/ob-main.js"></script>
+<script src="custom.js"></script>
+</body>
+</html>

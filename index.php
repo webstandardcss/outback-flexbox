568e63d5fc23c3339d3ddd932ee68662470759ed
webstandardcss <webstandardcss@gmail.com>
2018-03-29 03:03:35 -0500

    large one column product layout

--- index.php
+++ index.php
@@ -11,8 +11,22 @@ $pageKeywords = $pageTitle . "" . $siteKeywords;
   <main class="ob-main flex-container">
     <h1>Product Layouts</h1>
     <div class="ob-products">
+      <div class="product-large">
+        <h2>Large One Column Layout</h2>
+        <div class="product-row flex-container">
+          <div class="product-media">
+            <img class="product-image" src="obf/products/La-Bandera-Qu-Cntrl_20140917-370x3702-300x300.jpg" alt="La Bandera" />
+          </div>
+          <div class="product-description">
+            <h3>LA BANDERA QUAIL CENTRAL FEEDER & WATER STATION</h3>
+            <p>Designed by Wildlife Biologist Gene Naquain. Features side panels to shield quail and limit pest access. Also adjustable water drippers and feed pan.</p>
+            <p>1000# Feed Capacity & 24 Gallon Water Capacity – $1825</p>
+          </div>
+        </div>
+      </div>
+
       <div class="product">
-        <h2>Water Stations</h2>
+        <h2>Regular Product Layout</h2>
         <div class="product-row flex-container-row">
           <div class="product-media">
             <img class="product-image" src="obf/products/La-Bandera-Qu-Cntrl_20140917-370x3702-300x300.jpg" alt="La Bandera" />

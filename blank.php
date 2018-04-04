52a6dd108604092da639444c30fe17dfac57426c
unknown <luis@rkvideo.tv>
2018-04-04 08:54:55 -0500

    Created About Us page

--- blank.php
+++ blank.php
@@ -1,18 +1,22 @@
 <?php require_once(dirname(__FILE__) . "/inc/config.php"); ?>
 
 <?php // Page data
-$pageTitle = "Homepage";
-$pageTitleTag = $siteName . " " . $pageTitle;
-$pageDescription = $siteName  . " " . $pageTitle;
+$pageTitle = "About Us";
+$pageTitleTag = $siteName . "Outback Feeders" . $pageTitle;
+$pageDescription = $siteName  . "Learn about Outback Feeders" . $pageTitle;
 $pageKeywords = $pageTitle . "" . $siteKeywords;
 ?>
 
 <?php require_once(dirname(__FILE__) . "/inc/header.php"); ?>
   <main class="ob-main flex-container">
-    <h1>Water Stations</h1>
-    <div class="product">
+    <h1>Welcome to Outback!</h1>
+    <p>It all began in 1981 with Rick Meritt building custom steel fences and buildings as 3-M Steel Construction. Named 3-M (three Meritts) to include his wife and son.</p>
+	<p>As he built all over Texas, Rick developed products that better suited his first love – Hunting. Hence, Outback Wildlife Feeders was created and began with a single deer feeder. The product line has expanded to include about four dozen hunting, fishing and ranch equipment items.</p>
+	<p>Outback products are “Built Right the First Time, to Last a Lifetime”, withstanding extreme weather, wind and wildlife abuse.</p>
+	<p>All Outback products are innovatively designed and built at the original location in Gilmer, Texas. Made with only high quality American steel and craftsmanship, all products are 100% Satisfaction Guaranteed.</p>
+	<div class="product">
       <div class="product-row flex-container">
-        <img class="product-image" src="obf/products/La-Bandera-Qu-Cntrl_20140917-370x3702-300x300.jpg" alt="La Bandera" />
+        <img class="product-image" src="obf/products/La-Bandera-Qu-Cntrl_20140917-370x3702-300x300.jpg" alt="Rick with deer" />
         <div class="product-description">
           <h3>LA BANDERA QUAIL CENTRAL FEEDER & WATER STATION</h3>
           <p>Designed by Wildlife Biologist Gene Naquain. Features side panels to shield quail and limit pest access. Also adjustable water drippers and feed pan.</p>
@@ -20,5 +24,8 @@ $pageKeywords = $pageTitle . "" . $siteKeywords;
         </div>
       </div>
     </div>
+    <h1>Lifetime Warranty</h1>
+	<p>10 year manufacturing defect. We will personally repair, replace or hire a competent repair team to come to your location. After 10 years we will repair at our location in Gilmer Texas. 100% Satisfaction</p>
+	<p>Numerous patents are held by Outback and include: 8201520, 7370605, 6920841, D575908, D599503, D602649, D603104, D603105, D604017, D603566, D624706, D624708, D622453, D624707, D636942, D629572, D629976, D629975, D624709, D630802, D630653, D647253, D642750, D681883. Additional patents are pending. U.S. trademarks included 3629190 & 3625066.</p>
   </main>
 <?php require_once(dirname(__FILE__) . "/inc/footer.php"); ?>

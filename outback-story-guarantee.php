812cb4915368f41ec1e52f77559d723d7f61ea5c
webstandardcss <webstandardcss@gmail.com>
2018-04-09 14:37:46 -0500

    renamed about us file

---
+++ outback-story-guarantee.php
@@ -0,0 +1,35 @@
+<?php require_once(dirname(__FILE__) . "/inc/config.php"); ?>
+
+<?php // Page data
+$pageTitle = "Homepage";
+$pageTitleTag = $siteName . " " . $pageTitle;
+$pageDescription = $siteName  . " " . $pageTitle;
+$pageKeywords = $pageTitle . "" . $siteKeywords;
+?>
+
+<?php require_once(dirname(__FILE__) . "/inc/header.php"); ?>
+  <main class="ob-main flex-container">
+    <h1>Welcome to Outback!</h1>
+    <p>It all began in 1981 with Rick Meritt building custom steel fences and buildings as 3-M Steel Construction. Named 3-M (three Meritts) to include his wife and son.</p>
+	<p>As he built all over Texas, Rick developed products that better suited his first love – Hunting. Hence, Outback Wildlife Feeders was created and began with a single deer feeder. The product line has expanded to include about four dozen hunting, fishing and ranch equipment items.</p>
+	<p>Outback products are “Built Right the First Time, to Last a Lifetime”, withstanding extreme weather, wind and wildlife abuse.</p>
+	<p>All Outback products are innovatively designed and built at the original location in Gilmer, Texas. Made with only high quality American steel and craftsmanship, all products are 100% Satisfaction Guaranteed.</p>
+    <div class="product">
+      <div class="product-row flex-container">
+        <img class="product-image" src="obf/...." alt="Rick Merit with deer" />
+        <div class="product-description">
+        </div>
+      </div>
+    </div>
+    <h1>Lifetime Warranty</h1>
+	<p>10 year manufacturing defect. We will personally repair, replace or hire a competent repair team to come to your location. After 10 years we will repair at our location in Gilmer Texas. 100% Satisfaction</p>
+	<p>Numerous patents are held by Outback and include: 8201520, 7370605, 6920841, D575908, D599503, D602649, D603104, D603105, D604017, D603566, D624706, D624708, D622453, D624707, D636942, D629572, D629976, D629975, D624709, D630802, D630653, D647253, D642750, D681883. Additional patents are pending. U.S. trademarks included 3629190 & 3625066.</p>
+	<div class="product">
+      <div class="product-row flex-container">
+        <img class="product-image" src="obf/...." alt="Sunset at the Merit ranch" />
+        <div class="product-description">
+        </div>
+      </div>
+    </div>
+  </main>
+<?php require_once(dirname(__FILE__) . "/inc/footer.php"); ?>

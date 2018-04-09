7578a1cae7c97c4d481c4b50727f1a07de11b48a
webstandardcss <webstandardcss@gmail.com>
2018-04-09 14:19:29 -0500

    content page The Doc

---
+++ the-doc.php
@@ -0,0 +1,47 @@
+<?php require_once(dirname(__FILE__) . "/inc/config.php"); ?>
+
+<?php // Page data
+$pageTitle = "";
+$pageTitleTag = $siteName . "Outback Feeders" . $pageTitle;
+$pageDescription = $siteName  . "" . $pageTitle;
+$pageKeywords = $pageTitle . "" . $siteKeywords;
+?>
+
+<?php require_once(dirname(__FILE__) . "/inc/header.php"); ?>
+  <main class="ob-main flex-container">
+    <h1>Content Layout</h1>
+    <article class="ob-content">
+      <section class="ob-section">
+        <h2>The Doc</h2>
+        <p>Outback Wildlife Feeders has paired up with ADD-Vantage Delivery System to bring you “The Doc“.  A dual purpose 1000# protein feeder that not only allows you to use it as a normal gravity flow feeder, but “The Doc” also allows you to apply a liquid continuously and uniformly to a feed with a new state-of-the-art method at the rate of 20ml of liquid per 2.5 lbs. of feed on demand.</p>
+        <p>1000# – $3650</p>
+      </section>
+	<div class="product">
+      <div class="product-row flex-container">
+        <img class="product-image" src="obf/..." alt="" />
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/yUAAoRjFS64" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        <div class="product-description">
+          	
+          	<p>Liquids that can be used in “The Doc“:
+			<p>Feed Supplements
+
+			<p>Antioxidants</p>
+
+			<p>Vitamins</p>
+
+			<p>Minerals</p>
+
+			<p>Enzymes</p>
+
+			<p>Flavorants</p>
+
+			<p>Medications</p>
+
+			<p>So whether you are in the treatment or preventative stage or you are wanting to attract more animals, “The Doc” has you covered.</p>
+			<h3>When pulling or moving feeders use extreme caution.  Feeders can be top heavy.  Also never pull when feeders are full.</h3>
+        </div>
+      </div>
+    </div>
+    
+  </main>
+<?php require_once(dirname(__FILE__) . "/inc/footer.php"); ?>

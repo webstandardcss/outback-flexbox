05baebf09d616779b6834a81798e0774fbc1ba7b
webstandardcss <webstandardcss@gmail.com>
2018-04-10 18:55:29 -0500

    Created Feed delivery systems profile page

---
+++ profile-feed-delivery-systems.php
@@ -0,0 +1,53 @@
+<?php require_once(dirname(__FILE__) . "/inc/config.php"); ?>
+
+<?php // Page data
+$pageTitle = "Feed delivery systems profile";
+$pageTitleTag = $siteName . "Outback Feeders" . $pageTitle;
+$pageDescription = $siteName  . "" . $pageTitle;
+$pageKeywords = $pageTitle . "" . $siteKeywords;
+?>
+
+<?php require_once(dirname(__FILE__) . "/inc/header.php"); ?>
+  <main class="ob-main flex-container">
+    <h1>Feed Delivery Systems</h1>
+     <div class="product">
+        <div class="product-row flex-container-row">
+          <div class="product-media">
+            <img class="product-image" src="obf/products/Amigo-correct_091714.jpg" alt="Amigo air-driven" />
+          </div>
+          <div class="product-description">
+            <h2>AMIGO AIR-DRIVEN FEED SYSTEM</h2>
+            <p>Receiver hitch compatible or from your truck bed – $1600</p>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/jvFsWOaZfcw" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+          </div>
+        </div>
+      </div>
+<br>
+<br>
+<br>
+    <div class="product-half">
+      <div class="product-row flex-container-row">
+         <div class="product-single">
+            <div class="product-media">
+              <img class="product-image" src="obf/products/The-PUP_20140917-1024x768.jpg" alt="The PUP" />
+            </div>
+            <div class="product-description">
+              <h2>THE PUP ROAD FEEDER</h2>
+              <p>Equipped with remote control “Eliminator” road feeder and easily tows with ATV.</p>
+
+              <p>• 600 Lb. Capacity – $2425</p>
+            </div>
+          </div>
+          <div class="product-single">
+            <div class="product-media">
+              <img class="product-image" src="obf/products/float-tires-on-Pardner-4M-784x1024.png" alt="Floatation Tires" />
+            </div>
+            <div class="product-description">
+              <h2>FLOATATION TIRES ON YOUR FEED DELIVERY TRAILER</h2>
+              <p>• Per Axle – $700</p>
+            </div>
+          </div>
+        </div>
+      </div>
+  </main>
+<?php require_once(dirname(__FILE__) . "/inc/footer.php"); ?>

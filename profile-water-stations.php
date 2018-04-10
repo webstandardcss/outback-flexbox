d8d6016a3c9980c52b24194d9d86a2e8632df4fa
webstandardcss <webstandardcss@gmail.com>
2018-04-10 18:44:19 -0500

    created water statiosn profile page

---
+++ profile-water-stations.php
@@ -0,0 +1,82 @@
+<?php require_once(dirname(__FILE__) . "/inc/config.php"); ?>
+
+<?php // Page data
+$pageTitle = "Corn Feeders Profile";
+$pageTitleTag = $siteName . "Outback Feeders" . $pageTitle;
+$pageDescription = $siteName  . "" . $pageTitle;
+$pageKeywords = $pageTitle . "" . $siteKeywords;
+?>
+
+<?php require_once(dirname(__FILE__) . "/inc/header.php"); ?>
+  <main class="ob-main flex-container">
+    <h1>Water Stations</h1>
+     <div class="product">
+        
+        <div class="product-row flex-container-row">
+          <div class="product-media">
+            <img class="product-image" src="obf/products/La-Bandera-Qu-Cntrl_20140917-670x450.jpg" alt="La Bandera " />
+          </div>
+          <div class="product-description">
+            <h2>LA BANDERA QUAIL CENTRAL FEEDER & WATER STATION</h2>
+            <p>Designed by Wildlife Biologist Gene Naquain. Features side panels to shield quail & limit pest access, and adjustable water drippers & feed pan.</p>
+
+            <p>• 1000 Lb. Feed Capacity & 24 Gallon Water Capacity – $1450</p>
+          </div>
+        </div>
+      </div>
+<br>
+<br>
+<br>
+    <div class="product-half">
+      <div class="product-row flex-container-row">
+         <div class="product-single">
+            <div class="product-media">
+              <img class="product-image" src="obf/products/Reg-Quail-Cntrl_20140918-1024x863.jpg" alt="Regulator Quail Central Feeder" />
+            </div>
+            <div class="product-description">
+              <h2>REGULATOR QUAIL CENTRAL FEEDER & WATER STATION</h2>
+              <p>collects rainfall & condensation</p>
+              <p>Equipped with an Eliminator spinner to regulate feed time & amount dispersed</p>
+              
+              <p>• 600 Lb. Feed Capacity & 8 Gallon Water Reservoir – $1450</p>
+              
+              <p>• 1000 Lb. Feed Capacity & 8 Gallon Water Reservoir – $1500</p>
+            </div>
+          </div>
+
+          <div class="product-single">
+            <div class="product-media">
+              <img class="product-image" src="obf/products/Rain-collect-wings-open-1024x672.jpg" alt="Wildlife Guzzler" />
+            </div>
+            <div class="product-description">
+              <h2>WILDLIFE GUZZLER WATER STATION</h2>
+              <p>Collects more rainfall & natural condensation for wildlife</p>
+              <p>• 500 Gallon Capacity (1” rainfall collects 45 gal.) – $2700</p>
+              <p>For information about NRCS cost-sharing on the Wildlife Guzzler contact your local NRCS office or Outback Wildlife Feeders.</p>
+            </div>
+          </div>
+
+           <div class="product-single">
+            <div class="product-media">
+              <img class="product-image" src="obf/products/Flipper-dumping-300x251.png" alt="Flipper" />
+              <img class="product-image" src="obf/products/Flipper-300x236.png" alt="Flipper" />
+            </div>
+            <div class="product-description">
+              <h2>FLIPPER WATER TROUGHN</h2>
+              <p>Save time emptying & cleaning.</p>
+              <p>Easily dumps in seconds to clean epoxy coated interior.</p>
+              <p>• 25 Gallon Capacity – $600</p>
+
+            </div>
+          </div>
+
+          <div class="product-single">
+            <div class="product-media">
+               </div>
+            <div class="product-description">
+             </div>
+          </div>
+        </div>
+      </div>
+  </main>
+<?php require_once(dirname(__FILE__) . "/inc/footer.php"); ?>

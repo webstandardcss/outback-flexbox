6a92e6ae49451ea91d1aa2eff0cbbdce53bc0fa8
webstandardcss <webstandardcss@gmail.com>
2018-04-10 18:22:56 -0500

    created corn feeders profile page

---
+++ profile-corn-feeders.php
@@ -0,0 +1,65 @@
+<?php require_once(dirname(__FILE__) . "/inc/config.php"); ?>
+
+<?php // Page data
+$pageTitle = "Corn Feeders Profile";
+$pageTitleTag = $siteName . "Outback Feeders" . $pageTitle;
+$pageDescription = $siteName  . "" . $pageTitle;
+$pageKeywords = $pageTitle . "" . $siteKeywords;
+?>
+
+<?php require_once(dirname(__FILE__) . "/inc/header.php"); ?>
+  <main class="ob-main flex-container">
+    <h1>Cornfeeders</h1>
+     <div class="product">
+        <h2>CF Corn Feeder</h2>
+        <div class="product-row flex-container-row">
+          <div class="product-media">
+            <img class="product-image" src="obf/products/4-CF-+-Rick-scene-670x450.jpg" alt="Corn Feeders" />
+          </div>
+          <div class="product-description">
+            <h2>CF Corn Feeder</h2>
+            <p>features THE TIMER 12-Volt Control Unit with 5 year warranty shown with optional fold-up cages</p>
+
+            <p>• 600 lb. capacity – $1800</p>
+
+            <p>• 1000 lb. capacity – $1900</p>
+
+            </p>• 2000 lb. capacity – $2000</p>
+
+            <p>• 3000 lb. capacity – $2250</p>
+          </div>
+        </div>
+      </div>
+<h3> Options for Corn Feeders</h3>
+    <div class="product-half">
+      <div class="product-row flex-container-row">
+         <div class="product-single">
+            <div class="product-media">
+              <img class="product-image" src="obf/products/1-XTENDA-TUBE-1024x771.jpg" alt="Extended tube protein adapter" />
+            </div>
+            <div class="product-description">
+              <h2>EXTENDED TUBE PROTEIN ADAPTER</h2>
+              <p>• 4 tubes maximize wildlife head count per feeding – $500</p>
+
+              <p>• TM Protein Adapter Tube – $200</p>
+
+              <p>• Fold-up Cage – $75</p>
+            </div>
+          </div>
+          <div class="product-single">
+            <div class="product-media">
+              <img class="product-image" src="obf/products/low-profile-image+cutout.png" alt="low profile corn feeder" />
+            </div>
+            <div class="product-description">
+              <h2>LOW PROFILE CORN FEEDER WITH PISTOLERO DIRECTIONAL SPINNER</h2>
+              <p>• 600 lb. capacity – $1525</p>
+
+              <p>• 1000 lb. capacity – $1575</p>
+
+              <p>• Pistolero Spinner Unit Only – $650</p>
+            </div>
+          </div>
+        </div>
+      </div>
+  </main>
+<?php require_once(dirname(__FILE__) . "/inc/footer.php"); ?>

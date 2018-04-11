bc225a5fbbf756d7f32d5794c79ee2a38081e671
webstandardcss <webstandardcss@gmail.com>
2018-04-11 15:48:45 -0500

    Created and finalized The Coontroller page

---
+++ the-coontroller.php
@@ -0,0 +1,37 @@
+<?php require_once(dirname(__FILE__) . "/inc/config.php"); ?>
+
+<?php // Page data
+$pageTitle = "";
+$pageTitleTag = $siteName . "Outback Feeders" . $pageTitle;
+$pageDescription = $siteName  . "" . $pageTitle;
+$pageKeywords = $pageTitle . "" . $siteKeywords;
+?>
+
+<?php require_once(dirname(__FILE__) . "/inc/header.php"); ?>
+  <main class="ob-main flex-container">
+    <h1>The Coontroller</h1>
+
+    <div class="product">
+        <div class="product-row flex-container-row">
+          <div class="product-media">
+            <img class="product-image" src="obf/Misc/No-racooon-155-300x236.png" alt="Half-back protein feeder" />
+          </div>
+          <div class="product-description">
+            <p>ELIMINATES COSTLY FEED WASTE</p>
+            <p>$90 each</p>
+            <p>$360 per set of 4</p>
+            
+          </div>
+        </div>\
+        <br>
+        <div class="product-row flex-container-row">
+         <div class="product-media">
+            <img class="product-image" src="obf/products/1coontroller-on-feeder1-910x1024.png" alt="Coontroller on Protein Deer Feeder" />
+          </div>
+          <div class="product-description">
+          </div>
+       </div>
+      </div>
+   	    
+  </main>
+<?php require_once(dirname(__FILE__) . "/inc/footer.php"); ?>

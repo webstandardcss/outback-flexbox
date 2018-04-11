2627040ce9ad83da6b13880c88028002f9c85b50
webstandardcss <webstandardcss@gmail.com>
2018-04-11 16:28:33 -0500

    Created and added Fish feeder page, this page is finalized

---
+++ fish-feeder-share-kropper.php
@@ -0,0 +1,57 @@
+<?php require_once(dirname(__FILE__) . "/inc/config.php"); ?>
+
+<?php // Page data
+$pageTitle = "";
+$pageTitleTag = $siteName . "Outback Feeders" . $pageTitle;
+$pageDescription = $siteName  . "" . $pageTitle;
+$pageKeywords = $pageTitle . "" . $siteKeywords;
+?>
+
+<?php require_once(dirname(__FILE__) . "/inc/header.php"); ?>
+  <main class="ob-main flex-container">
+    <h1>Fish Feederck!</h1>
+    <h2>Introducing Brand New Fish Feeder Designs</h2>
+    <h1>The “Share-Kropper”</h1>
+    
+     <div class="product">
+        <div class="product-row flex-container-row">
+          <div class="product-media">
+            <img class="product-image" src="obf/products/Share-Kropper-1-878x1024.png" alt="Share-Kropper" />
+          </div>
+          <div class="product-description">
+            <p>With all the same rugged durability that comes standard from Outback Wildlife Feeders, we proudly introduce the  “Share-Kropper”. This awesome low profile directional feeder works equally well with all types of fish feed, corn or milo. The Share-Kropper can be placed on a pier, dock, bank, or truck to out-perform and outlast all those tin can feeders out there.<p>
+           <br>
+           <p> • 24″ tank diameter holds 175# floating fish feed or 300# corn</p>
+           <p>• Spreads heavier feed out to 50′ in a 20′ wide pattern</p>
+           <p>• Directional spreader can go up to 360°</p>
+           <p>• 54″ tall & 3’X3′ at base</p>
+           <p>• 141# empty weight</p>
+           <br>
+           <br>
+           <p>• With Outback’s Directional Spinner or the Eliminator 360 degree spinner both are the same price of $1100.00</p>
+          </div>
+        </div>
+      </div>
+
+    <div class="product-half">
+        <div class="product-row flex-container-row">
+          <div class="product-single">
+            <div class="product-media">
+              <img class="product-image" src="obf/Hunting Photos/Directional-300x300.png" alt="Directional" />
+            </div>
+            <div class="product-description">
+              <h3> Directional</h3>
+            </div>
+          </div>
+          <div class="product-single">
+            <div class="product-media">
+              <img class="product-image" src="obf/Hunting Photos/Eliminator-300x300.jpg" alt="Eliminator" />
+            </div>
+            <div class="product-description">
+              <h3>Eliminator</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+  </main>
+<?php require_once(dirname(__FILE__) . "/inc/footer.php"); ?>

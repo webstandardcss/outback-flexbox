47eedcf0811240b7edafaa1aac3d4ceba940baee
webstandardcss <webstandardcss@gmail.com>
2018-04-11 17:24:24 -0500

    Created and finalized Water Stations page

---
+++ water-stations.php
@@ -0,0 +1,92 @@
+<?php require_once(dirname(__FILE__) . "/inc/config.php"); ?>
+
+<?php // Page data
+$pageTitle = "";
+$pageTitleTag = $siteName . "Outback Feeders" . $pageTitle;
+$pageDescription = $siteName  . "" . $pageTitle;
+$pageKeywords = $pageTitle . "" . $siteKeywords;
+?>
+
+<?php require_once(dirname(__FILE__) . "/inc/header.php"); ?>
+  <main class="ob-main flex-container">
+    <h1>Water Stations</h1>
+
+     <div class="product">
+        <div class="product-row flex-container-row">
+          <div class="product-media">
+            <img class="product-image" src="obf/products/La-Bandera-Qu-Cntrl_20140917-370x3702-300x300.jpg" alt=""  />            
+          </div>
+          <div class="product-description">
+            <h3>LA BANDERA QUAIL CENTRAL FEEDER & WATER STATION</h3>  
+
+            <p>Designed by Wildlife Biologist Gene Naquain. Features side panels to shield quail & limit pest access.  Adjustable water drippers & feed pan.</p>
+
+            <p>1000#  Feed Capacity & 24 Gallon Water Capacity – $1825</p>
+
+          </div>
+        </div>
+      </div>
+
+    <div class="product">
+        <div class="product-row flex-container-row">
+          <div class="product-media">
+            <img class="product-image" src="obf/products/Reg-Quail-Cntrl_20140918-300x252.jpg" alt=""  />            
+          </div>
+          <div class="product-description">
+            <h3>REGULATOR QUAIL CENTRAL FEEDER & WATER STATION</h3>  
+
+            <p>Collects rainfall & condensation. </br>
+            Equipped with an Eliminator Spinner to regulate feed, time and amount dispersed.</p>
+
+            <p>600# Feed Capacity & 8 Gallon Water Reservoir $1625</p>
+
+            <p>1000# Feed Capacity & 8 Gallon Water Reservoir $1775</p>
+
+          </div>
+        </div>
+      </div>
+
+    <div class="product">
+        <div class="product-row flex-container-row">
+          <div class="product-media">
+            <img class="product-image" src="obf/products/IMG_5273-e1516211144778-300x290.png" alt="Flipper Water Trough"  />            
+          </div>
+          <div class="product-description">
+            <h3>FLIPPER WATER TROUGH</h3>
+
+            <p>Save time emptying & cleaning. Easily dumps in seconds to clean NEW Roto Molded Trough.
+            25 Gallon Capacity – $575.</p>
+
+          </div>
+        </div>
+      </div>
+
+      <div class="product">
+        <div class="product-row flex-container-row">
+          <div class="product-media">
+            <img class="product-image" src="obf/products/Rain-Collect-wings-open-300x197.jpg" alt="Wildlife Guzzler"  />            
+          </div>
+          <div class="product-description">
+            <h3>WILDLIFE GUZZLER WATER STATION</h3>
+
+            <p>Collects more rainfall and natural condensation for wildlife.<br>
+            500 Gallon Capacity (1” rainfall collects 45 gal.)</p>
+
+            <p>$3150</p>
+
+            <p>For information about NRCS cost-sharing on the Wildlife Guzzler contact your local NRCS office or Outback Wildlife Feeders.</p>
+          </div>
+        </div>
+      </div>
+
+    <div class="product">
+     <div class="product-row flex-container-row">
+      <div class="product-media">
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/n0jeQUP-aeo" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+          </div>
+        </div>
+      </div>
+
+     
+  </main>
+<?php require_once(dirname(__FILE__) . "/inc/footer.php"); ?>

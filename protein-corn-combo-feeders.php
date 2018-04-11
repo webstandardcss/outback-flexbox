6763953883ff9ae0114c1d1a45f8a208c8d102be
webstandardcss <webstandardcss@gmail.com>
2018-04-11 16:08:30 -0500

    created and edited Protein Corn Combo feeder page, this page is now finalzed

---
+++ protein-corn-combo-feeders.php
@@ -0,0 +1,60 @@
+<?php require_once(dirname(__FILE__) . "/inc/config.php"); ?>
+
+<?php // Page data
+$pageTitle = "Protein Feeders";
+$pageTitleTag = $siteName . "Outback Feeders" . $pageTitle;
+$pageDescription = $siteName  . "Outback Feeders Protein Feeders and options" . $pageTitle;
+$pageKeywords = $pageTitle . "Protein Feeders gravity flow regulator" . $siteKeywords;
+?>
+
+<?php require_once(dirname(__FILE__) . "/inc/header.php"); ?>
+  <main class="ob-main flex-container">
+    <h1>Protein & Corn Combo Feeders </h1>
+
+	<div class="product">
+      <div class="product-row flex-container-row">
+      	<div class="product-media">
+        <img class="product-image" src="obf/products/IMG_0752.jpg" alt="Dually" />
+    </div>
+        <div class="product-description">
+          <h3>The “DUALLY” Protein & Corn Feeder</h3>
+
+          <p>1000# – $2500 <br>
+           (250# Corn & 750# Protein)</p>
+
+          <p>2000# – $2600 <br>
+          (500# Corn & 1500# Protein)</p>
+
+          <p>3000# – $2900<br>
+          (750# Corn & 2250# Protein)</p>
+
+          <p>Bear Cages – $250</p>
+        </div>
+        <p>Features split hopper that allows for gravity-flow protein simultaneously with directional corn<br> distribution controlled by “The Timer” 12-Volt Control Unit with 5 year warranty.<br>
+        Comes standard with three protein tubes. Available with up to six tubes and in three capacities</p>
+      </div>
+    </div>
+<br>
+    <div class="product">
+      <div class="product-row flex-container-row">
+      	<div class="product-media">
+        <img class="product-image" src="obf/products/REGULATOR-DUALLY_20140916-720x1024.jpg" alt="Regulator Dually "  />
+        </div>
+        <div class="product-description">
+          <h3>The REGULATOR DUALLY</h3>
+
+          <p>Combines all the great features of the Regulator <br> and the Dually</p>
+
+         <p>2000# – $3400 <br>
+          3000# – $3750</p>
+        </div>
+      </div>
+    </div>
+<br>
+<br>
+    <iframe width="940" height="529" src="https://www.youtube.com/embed/fiJyYeie-ng" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+<br>
+<br>
+ 
+  </main>
+<?php require_once(dirname(__FILE__) . "/inc/footer.php"); ?>

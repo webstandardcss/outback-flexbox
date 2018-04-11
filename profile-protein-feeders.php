7bdf1f77eae3bd2535df63b6365ebd608eb040a6
webstandardcss <webstandardcss@gmail.com>
2018-04-10 19:18:28 -0500

    Created Protein feeders profile page

---
+++ profile-protein-feeders.php
@@ -0,0 +1,129 @@
+<?php require_once(dirname(__FILE__) . "/inc/config.php"); ?>
+
+<?php // Page data
+$pageTitle = "Protein Feeders Profile";
+$pageTitleTag = $siteName . "Outback Feeders" . $pageTitle;
+$pageDescription = $siteName  . "" . $pageTitle;
+$pageKeywords = $pageTitle . "" . $siteKeywords;
+?>
+
+<?php require_once(dirname(__FILE__) . "/inc/header.php"); ?>
+  <main class="ob-main flex-container">
+    <h1>Protein feeders</h1>
+     <div class="product">
+        <div class="product-row flex-container-row">
+          <div class="product-media">
+            <img class="product-image" src="obf/products/3-CPs-scene-670x450.jpg" alt="Protein Feeders" />
+          </div>
+          <div class="product-description">
+            <h2>CP GRAVITY FLOW PROTEIN FEEDER</h2>
+            
+            <p>• 1000 lb. capacity – $1900</p>
+
+            <p>• 2000 lb. capacity – $2000</p>
+
+            <p>• 3000 lb. capacity – $2250</p>
+          </div>
+        </div>
+      </div>
+
+<br>
+<br>
+<br>
+
+    <div class="product-half">
+      <div class="product-row flex-container-row">
+         <div class="product-single">
+            <div class="product-media">
+              <img class="product-image" src="obf/products/CP-6-tube-914x1024.jpg" alt="CP 6-tube" />
+            </div>
+            <div class="product-description">
+              <h2>CP 6-TUBE PROTEIN FEEDER</h2>
+              <h3>with Removable Ladder</h3>
+             <p>• 1000 lb. capacity – $2075</p>
+
+            <p>• 2000 lb. capacity – $2175</p>
+
+            <p>• 3000 lb. capacity – $2400</p>
+            </div>
+          </div>
+          
+          <div class="product-single">
+            <div class="product-media">
+              <img class="product-image" src="obf/products/CP-6-tube-914x1024.jpg" alt="CP 6-tube" />
+            </div>
+            <div class="product-description">
+              <h2>CP BREEDING PEN FEEDER</h2>
+              <p>The CP Breeding Pen Feeder is designed to position alongside fence to fill from outside pen.</p>
+              <p>The removable ladder attaches through the fence.</p>
+              <p>• 1000 lb. capacity – $1900</p>
+
+              <p>• 2000 lb. capacity – $2000</p>
+
+              <p>• 3000 lb. capacity – $2250</p>
+            </div>
+          </div>
+        
+          <div class="product-single">
+            <div class="product-media">
+              <img class="product-image" src="obf/products/TM-PROTEIN-FDR-768x1024.jpg" alt="TM PROTEIN FDR" />
+            </div>
+            <div class="product-description">
+              <h2>TM PROTEIN FEEDER</h2>
+              <p>• 1000 lb. capacity – $1800</p>
+
+              <p>• 2000 lb. capacity – $1900</p>
+
+              <p>• 3000 lb. capacity – $2150</p>
+            </div>
+          </div>
+
+          <div class="product-single">
+            <div class="product-media">
+              <img class="product-image" src="obf/products/Magnum-detail.jpg" alt="Magnum Detail" />
+            </div>
+            <div class="product-description">
+              <h2>CP Magnum Feeder</h2>
+              <h3>Features Larger Ports for Larger Snout Game</h3>
+
+              <p>• 1000 lb. capacity – $2075</p>
+
+              <p>• 2000 lb. capacity – $2175</p>
+
+              <p>• 3000 lb. capacity – $2400</p>
+
+              <h2>CP 6-TUBE MAGNUM FEEDER</h2>
+              
+              <p>• 1000 lb. capacity – $2350</p>
+
+              <p>• 2000 lb. capacity – $2450</p>
+
+              <p>• 3000 lb. capacity – $2700</p>
+            </div>
+          </div>
+
+          <div class="product-single">
+            <div class="product-media">
+              <img class="product-image" src="obf/products/OB-lumberjack1.jpg" alt="OB Lumberjack" />
+            </div>
+            <div class="product-description">
+              <h2>Lumberjack</h2>
+              <p>200 Lb. Capacity – $875</p>
+            </div>
+          </div>
+
+          <div class="product-single">
+            <div class="product-media">
+              <img class="product-image" src="obf/products/Mineral-Fdr-Attachment-612x1024.jpg" alt="Mineral FDR Attachment" />
+            </div>
+            <div class="product-description">
+              <h2>GRANULAR MINERAL FEEDER</h2>
+              <p>• Easily attaches to the leg of any Outback feeder </p>
+              <p>• 75 Lb. Capacity – $325</p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+  </main>
+<?php require_once(dirname(__FILE__) . "/inc/footer.php"); ?>

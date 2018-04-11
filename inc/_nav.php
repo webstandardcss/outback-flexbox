3abba3754e53d4a6ca9450c24be7043dc051efd6
webstandardcss <webstandardcss@gmail.com>
2018-04-11 14:01:19 -0500

    Updated the Main menu to use all the new php files

--- inc/_nav.php
+++ inc/_nav.php
@@ -5,7 +5,7 @@
       <a href="./outback-story-guarantee.php" class="sf-with-ul">About Us<span class="sf-sub-indicator"> »</span></a>
       <ul class="sf-dropdown-menu">
         <li class="menu-contact-us menu-contact-us"><a href="./contact-us/">Contact Us</a></li>
-        <li class="menu-delivery-information menu-delivery-information"><a href=".//delivery-information.php">Delivery Information</a></li>
+        <li class="menu-delivery-information menu-delivery-information"><a href="./delivery-information.php">Delivery Information</a></li>
         <li class="menu-outback-story-guarantee menu-outback-story-guarantee"><a href="./outback-story-guarantee.php">Outback Story &amp; Guarantee</a></li>
       </ul>
     </li>
@@ -14,37 +14,37 @@
       <a href="./corn-feeders.php" class="sf-with-ul">Corn Feeders<span class="sf-sub-indicator"> »</span></a>
       <ul class="sf-dropdown-menu"">
         <li class="menu-baby-back-low-profile-feeders menu-baby-back-low-profile-feeders"><a href="./baby-back-feeder-series.php">BABY-BACK Low Profile Feeders</a></li>
-        <li class="menu-top-gun-corn-feeder menu-top-gun-corn-f\eeder"><a href="./top-gun-corn-feeder/">Top Gun Corn Feeder</a></li>
+        <li class="menu-top-gun-corn-feeder menu-top-gun-corn-f\eeder"><a href="./top-gun-corn-feeder.php">Top Gun Corn Feeder</a></li>
       </ul>
     </li>
     <li class="menu-protein-feeders sf-dropdown menu-protein-feeders">
-      <a href="http://outbackfeeders.com/protein-feeders/" class="sf-with-ul">Protein Feeders<span class="sf-sub-indicator"> »</span></a>
+      <a href="./protein-feeders.php" class="sf-with-ul">Protein Feeders<span class="sf-sub-indicator"> »</span></a>
       <ul class="sf-dropdown-menu">
         <li class="menu-half-back-protein-feeder menu-half-back-protein-feeder"><a href="./half-back-protein.php">Half-Back Protein Feeder</a></li>
         <li class="menu-the-doc menu-the-doc"><a href="./the-doc.php">The Doc</a></li>
-        <li class="menu-the-coontroller menu-the-coontroller"><a href="http://outbackfeeders.com/the-coontroller/">THE COONTROLLER</a></li>
+        <li class="menu-the-coontroller menu-the-coontroller"><a href="./the-coontroller.php">THE COONTROLLER</a></li>
       </ul>
     </li>
-    <li class="menu-protein-corn-combo-feeders menu-protein-corn-combo-feeders"><a href="http://outbackfeeders.com/protein-corn-combo-feeders/">Protein &amp; Corn Combo Feeders</a></li>
-    <li class="menu-fish-feeder menu-fish-feeder"><a href="http://outbackfeeders.com/fish-feeder-share-kropper/">Fish Feeder</a></li>
-    <li class="menu-quail-turkey-feeders active menu-quail-turkey-feeders"><a href="http://outbackfeeders.com/quail-turkey-feeders/">Quail &amp; Turkey Feeders</a></li>
-    <li class="menu-water-stations menu-water-stations"><a href="http://outbackfeeders.com/water-stations/">Water Stations</a></li>
+    <li class="menu-protein-corn-combo-feeders menu-protein-corn-combo-feeders"><a href="./protein-corn-combo-feeders.php">Protein &amp; Corn Combo Feeders</a></li>
+    <li class="menu-fish-feeder menu-fish-feeder"><a href="./fish-feeder-share-kropper.php">Fish Feeder</a></li>
+    <li class="menu-quail-turkey-feeders active menu-quail-turkey-feeders"><a href="./quail-turkey-feeders.php">Quail &amp; Turkey Feeders</a></li>
+    <li class="menu-water-stations menu-water-stations"><a href="./water-stations.php">Water Stations</a></li>
     <li class="menu-feed-delivery-systems sf-dropdown menu-feed-delivery-systems">
-      <a href="http://outbackfeeders.com/feed-delivery-systems/" class="sf-with-ul">Feed Delivery Systems<span class="sf-sub-indicator"> »</span></a>
+      <a href="./feed-delivery-systems.php" class="sf-with-ul">Feed Delivery Systems<span class="sf-sub-indicator"> »</span></a>
       <ul class="sf-dropdown-menu">
-        <li class="menu-the-road-doc menu-the-road-doc"><a href="http://outbackfeeders.com/new-products/">THE ROAD DOC</a></li>
-        <li class="menu-bulk-trailers menu-bulk-trailers"><a href="http://outbackfeeders.com/bulk-feed-systems/bulk-trailers/">Bulk Trailers</a></li>
-        <li class="menu-bulk-hoppers menu-bulk-hoppers"><a href="http://outbackfeeders.com/bulk-feed-systems/bulk-hoppers/">Bulk Hoppers</a></li>
-        <li class="menu-bulk-bins menu-bulk-bins"><a href="http://outbackfeeders.com/bulk-feed-systems/bulk-bins/">Bulk Bins</a></li>
+        <li class="menu-the-road-doc menu-the-road-doc"><a href="./new-products.php">THE ROAD DOC</a></li>
+        <li class="menu-bulk-trailers menu-bulk-trailers"><a href="./bulk-trailers.php">Bulk Trailers</a></li>
+        <li class="menu-bulk-hoppers menu-bulk-hoppers"><a href="./bulk-hoppers.php">Bulk Hoppers</a></li>
+        <li class="menu-bulk-bins menu-bulk-bins"><a href="./bulk-bins.php">Bulk Bins</a></li>
       </ul>
     </li>
-    <li class="menu-traps-pens menu-traps-pens"><a href="http://outbackfeeders.com/traps-pens/">Traps &amp; Pens</a></li>
-    <li class="menu-hunt-camp-equipment menu-hunt-camp-equipment"><a href="http://outbackfeeders.com/hunt-camp-equipment/">Hunt &amp; Camp Equipment</a></li>
-    <li class="menu-specialty-products menu-specialty-products"><a href="http://outbackfeeders.com/specialty-products/">Specialty Products</a></li>
-    <li class="menu-pre-owned-feeders menu-pre-owned-feeders"><a href="http://outbackfeeders.com/pre-owned-feeders/">Pre Owned Feeders</a></li>
-    <li class="menu-trophy-gallery menu-trophy-gallery"><a href="http://outbackfeeders.com/trophy-gallery/">Trophy Gallery</a></li>
-    <li class="menu-become-a-dealer menu-become-a-dealer"><a href="http://outbackfeeders.com/become-a-dealer/"><i class="highlight-button"></i>Become a Dealer</a></li>
-    <li class="menu-outback-dealer-list menu-outback-dealer-list"><a href="http://outbackfeeders.com/about-us/outback-dealers/">Outback Dealer List</a></li>
+    <li class="menu-traps-pens menu-traps-pens"><a href="./traps-pens.php">Traps &amp; Pens</a></li>
+    <li class="menu-hunt-camp-equipment menu-hunt-camp-equipment"><a href="./hunt-camp-equipment.php">Hunt &amp; Camp Equipment</a></li>
+    <li class="menu-specialty-products menu-specialty-products"><a href="./specialty-products.php">Specialty Products</a></li>
+    <li class="menu-pre-owned-feeders menu-pre-owned-feeders"><a href="./pre-owned-feeders.php">Pre Owned Feeders</a></li>
+    <li class="menu-trophy-gallery menu-trophy-gallery"><a href="./trophy-gallery.php">Trophy Gallery</a></li>
+    <li class="menu-become-a-dealer menu-become-a-dealer"><a href="./become-a-dealer.php"><i class="highlight-button"></i>Become a Dealer</a></li>
+    <li class="menu-outback-dealer-list menu-outback-dealer-list"><a href="./outback-dealers.php">Outback Dealer List</a></li>
     <li class="menu-kickin-back-kreations menu-kickin-back-kreations"><a href="http://KickinBackKreations.com">Kickin’ Back Kreations</a></li>
   </ul>
 </nav>

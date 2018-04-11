5b14c4d4a617b77b6832f24a57aedf1b0355dd70
webstandardcss <webstandardcss@gmail.com>
2018-04-11 17:12:03 -0500

    Created and finalized the Quail Turkey Feeder page

---
+++ quail-turkey-feeders.php
@@ -0,0 +1,138 @@
+<?php require_once(dirname(__FILE__) . "/inc/config.php"); ?>
+
+<?php // Page data
+$pageTitle = "";
+$pageTitleTag = $siteName . "Outback Feeders" . $pageTitle;
+$pageDescription = $siteName  . "" . $pageTitle;
+$pageKeywords = $pageTitle . "" . $siteKeywords;
+?>
+
+<?php require_once(dirname(__FILE__) . "/inc/header.php"); ?>
+  <main class="ob-main flex-container">
+    <h1>Quail & Turkey Feeders</h1>
+     <div class="product">
+        <div class="product-row flex-container-row">
+          <div class="product-media">
+            <img class="product-image" src="obf/products/3-low-profile-TUR-QU_20140917-1024x404.jpg" alt="3 low profile TUR-QU"  />
+          </div>
+          <div class="product-description">
+            <h3>QUAIL FEEDER (left)</h3>
+
+          <p>600# – $1150<br>
+          1000# – $1250</p>
+
+          <h3>TURKEY-QUAIL COMBO FEEDER (center)</h3>
+
+          <p>600# – $1150<br>
+          1000# – $1250</p>
+
+          <h3>QUAIL CENTRAL FEEDER & WATER STATION</h3>
+
+          <p>Collects Rainfall & Condensation<br>
+          600# – $1400<br>
+          1000# – $1550</p>
+          </div>
+        </div>
+      </div>
+
+    <div class="product">
+        <div class="product-row flex-container-row">
+          <div class="product-media">
+            <img class="product-image" src="obf/products/IMG_0913-1-300x225.jpg" alt=""  />            
+          </div>
+          <div class="product-media">
+          <img class="product-image" src="obf/products/IMG_0911-1-225x300.jpg" alt=""  />
+          </div>
+          <div class="product-description">
+            <h3>Regulated Turkey Feeder</h3>
+
+          <p>600# – $1400</p>
+          <p>1000# – $1550</p>
+
+          </div>
+        </div>
+      </div>
+
+       <div class="product">
+        <div class="product-row flex-container-row">
+          <div class="product-media">
+            <img class="product-image" src="obf/products/La-Bandera-Qu-Cntrl_20140917-370x3702-300x300.jpg" alt=""  />            
+          </div>
+          <div class="product-description">
+            <h3>LA BANDERA QUAIL CENTRAL FEEDER & WATER STATION</h3>  
+
+            <p>Designed by Wildlife Biologist Gene Naquain. Features side panels to shield quail & limit pest access.  Adjustable water drippers & feed pan.</p>
+
+            <p>1000#  Feed Capacity & 24 Gallon Water Capacity – $1825</p>
+
+          </div>
+        </div>
+      </div>
+
+<div class="product">
+        <div class="product-row flex-container-row">
+          <div class="product-media">
+            <img class="product-image" src="obf/products/Reg-Quail-Cntrl_20140918-300x252.jpg" alt=""  />            
+          </div>
+          <div class="product-description">
+            <h3>REGULATOR QUAIL CENTRAL FEEDER & WATER STATION</h3>  
+
+            <p>Collects rainfall & condensation. </br>
+            Equipped with an Eliminator Spinner to regulate feed, time and amount dispersed.</p>
+
+            <p>600# Feed Capacity & 8 Gallon Water Reservoir $1625</p>
+
+            <p>1000# Feed Capacity & 8 Gallon Water Reservoir $1775</p>
+
+          </div>
+        </div>
+      </div>
+
+<div class="product">
+        <div class="product-row flex-container-row">
+          <div class="product-media">
+            <img class="product-image" src="obf/products/1quail-tender-225x300.jpg" alt=""  />            
+          </div>
+          <div class="product-description">
+            <h3>QUAIL TENDER TRAILER</h3>
+
+            <p>Features Outback Pistolero Directional Spinner Unit positioned to scatter feed out the side into the brush from the sendero while driving.</p>
+
+            <p>2000# – $5125<br>
+            3000# – $5375</p>
+          </div>
+        </div>
+      </div>
+
+<div class="product">
+        <div class="product-row flex-container-row">
+          <div class="product-media">
+            <img class="product-image" src="obf/products/OWF-Quail-Block-Feeder-300x241.jpg" alt=""  />            
+          </div>
+          <div class="product-media">
+          <img class="product-image" src="obf/products/OWF-Quail-Block-Feeder-closeup-300x225.jpg" alt=""  />
+          </div>
+          <div class="product-description">
+            <h3>Regulated Turkey Feeder</h3>
+
+          <p>Protects blocks from undesirable varmints and provides much needed water for quail. Water system collects rainfall and condensation. Features heavy-duty construction and is easily movable.
+          28-gal water capacity<br>
+          $1500</p>
+         </div>
+        </div>
+      </div>
+
+ <div class="product-reverse">
+        <div class="product-row flex-container-row flex-container-row-reverse">
+          <div class="product-media">
+            <img class="product-image" src="obf/products/image1-3-240x300.jpg" alt="La Bandera" />
+          </div>
+          <div class="product-description">
+            <h3>BABY BACK 300 QUAIL FEEDER</h3>
+            <p>This new compact quail feeder features a 300# hopper with large feed tray and rain shields to keep your feed dry. The feed area is completely caged from top to bottom to insure your quail will have feed when they want it. Designed with 100% all steel construction to give you years of worry free use.</p>
+            <p>$725</p>
+          </div>
+        </div>
+      </div>
+  </main>
+<?php require_once(dirname(__FILE__) . "/inc/footer.php"); ?>

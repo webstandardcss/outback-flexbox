8e16620a34ae8ad025699bd4f5620c13257645ef
webstandardcss <webstandardcss@gmail.com>
2018-04-12 16:57:41 -0500

    created anf finalized Trap and Pens page

---
+++ traps-pens.php
@@ -0,0 +1,89 @@
+<?php require_once dirname(__FILE__) . "/inc/config.php"; ?>
+
+<?php // Page data
+$pageTitle = "Traps & Pens";
+$pageTitleTag = $siteName . "Traps and Pens" . $pageTitle;
+$pageDescription = $siteName  . "Outback Feeders Traps and Pens" . $pageTitle;
+$pageKeywords = $pageTitle . "Protein Feeders gravity flow regulator" . $siteKeywords;
+?>
+
+<?php require_once dirname(__FILE__) . "/inc/header.php"; ?>
+  <main class="ob-main flex-container">
+    <h1>Traps & Pens </h1>
+
+	<div class="product">
+      <div class="product-row flex-container-row">
+      	<div class="product-media">
+        <img class="product-image" src="obf/products/Fdr-Pen-+-gate-1024x750.png" alt="FDR Pen & Gate" width="400" height="300" />
+    </div>
+        <div class="product-description">
+          <h3>Feeder Pens</h3>
+          <p>You customize your shape and size of pen and even change location as needed. Portable panels are 3’ x 20’ and easily pin together with rods.</p>
+
+          <p>Features 4” x 4” welded wire panel on 1-1/4” pipe frame and 3’ wide gate section.</p>
+
+          <p>Panels – $400<br>
+          Gate Section – $275</p>
+
+        </div>
+      </div>
+    </div>
+<br>
+    <div class="product">
+      <div class="product-row flex-container-row">
+      	<div class="product-media">
+        <img class="product-image" src="obf/products/hog-trap-open-dr-300x295.png" alt="Hog trap open door" />
+        </div>
+        <div class="product-description">
+          <h1>Hog Traps</h1>
+          <p>Heavy Duty Hog Trap – $1250</p>
+          <p>With Steel floor – $1600</p>
+          <p>With Optional Swivel Axle – $1850</p>
+        </div>
+      </div>
+    </div>
+<br>
+<br>
+<br>
+    <div class="product-half">
+        <div class="product-row flex-container-row">
+          <div class="product-single">
+            <div class="product-media">
+              <img class="product-image" src="obf/products/Hog-Trap-axle-floor-300x221.png" alt="Hog Trap axle floor" />
+            </div>
+            <div class="product-description">
+              <h3>Hog Trap</h3>
+              <p>With Swivel Axle & Floor</p>
+              <p>$2150</p>
+            </div>
+          </div>
+          <div class="product-single">
+            <div class="product-media">
+              <img class="product-image" src="obf/products/Sectnal-Hog-Trap-NO-PAINT-300x158.jpg" alt="La Bandera" />
+            </div>
+            <div class="product-description">
+              <h3>Sectional Heavy Duty Hog Trap</h3>
+              <p>Four panels, 10’ x 10’ square with trip gate included.</p>
+              <p>(Not Painted)  $1700</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    <div class="product">
+      <div class="product-row flex-container-row">
+        <div class="product-description">
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/diG93aIbOfY" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
+      </div>
+    </div>
+   
+   	<br>
+   	<br>
+	<br>
+
+    
+    </div>
+</div>
+  </main>
+<?php require_once dirname(__FILE__) . "/inc/footer.php"; ?>

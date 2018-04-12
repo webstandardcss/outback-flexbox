e4717c3710af04efa905e809755cac0287d9ca4a
webstandardcss <webstandardcss@gmail.com>
2018-04-12 17:36:02 -0500

    created and finalized the hunt and camp equipment page

---
+++ hunt-camp-equipment.php
@@ -0,0 +1,145 @@
+<?php require_once dirname(__FILE__) . "/inc/config.php"; ?>
+
+<?php // Page data
+$pageTitle = "Protein Feeders";
+$pageTitleTag = $siteName . "Outback Feeders" . $pageTitle;
+$pageDescription = $siteName  . "Outback Feeders Protein Feeders and options" . $pageTitle;
+$pageKeywords = $pageTitle . "Protein Feeders gravity flow regulator" . $siteKeywords;
+?>
+
+<?php require_once dirname(__FILE__) . "/inc/header.php"; ?>
+  <main class="ob-main flex-container">
+    <h1>Hunt & Camp Equipment</h1>
+
+	<div class="product">
+      <div class="product-row flex-container-row">
+      	<div class="product-media">
+        <img class="product-image" src="obf/products/Gril_page-1024x896.png" alt="Kicking Back Kreations Grills" width="450" height="493" />
+    </div>
+        <div class="product-description">
+          <h3>Grills & Fire Pits</h3>
+          <p>24” TAILGATER FIRE PIT (Top Center)</p>
+          <p>Includes fire poker</p>
+          <p>$525</p>
+          <p>30” JUNIOR FIRE PIT (Left)</p>
+          <p>Includes swivel grill, Dutch oven support, fire poker & ash pan</p>
+          <p>$775</p>
+          <p>36” TEXAS SIZE FIRE PIT (RIGHT)</p>
+          <p>Includes fire poker</p>
+          <p>$975</p>
+        </div>
+      </div>
+    </div>
+<br>
+<br>
+
+    <div class="product">
+      <div class="product-row flex-container-row">
+      	<div class="product-media">
+        <img class="product-image" src="obf/products/shooting-bench-300x300.png" alt="SHOOTING BENCH" />
+        </div>
+        <div class="product-description">
+          <h3>SHOOTING BENCH</h3>
+          <p>Portable with swivel seat and rubber mat<br>
+          $550</p>
+        </div>
+      </div>
+    </div>
+
+  <br>
+  <br>
+
+    <div class="product">
+      <div class="product-row flex-container-row">
+      	<div class="product-media">
+        <img class="product-image" src="obf/products/Game-Rack-222x300.jpg" alt="Game Rack" />
+    	</div>
+        <div class="product-description">
+          <h3>Game Rack</h3>
+          <p>Portable with LED lighting and 4 winches</p>
+
+          <p>800# capacity each</p>
+
+          <p>$1125</p>
+        </div>
+      </div>
+    </div>
+    
+    <br>
+    <br>
+
+    <div class="product">
+      <div class="product-row flex-container-row">
+       <div class="product-media">
+        <img class="product-image" src="obf/products/EZ-lift-on-truck-187x300.jpg" alt="EZ lift on truck" />
+    	</div>
+        <div class="product-description">
+          <h3>E-Z LIFT LOADER</h3>
+          <p>Locking swivel design allows easy loading of animal from ground or vehicle</p>
+
+          <p>Features heavy-duty brake</p>
+          <p>$425</p>
+        </div>
+      </div>
+    </div>
+
+    <br>
+    <br>
+
+    <div class="product">
+      <div class="product-row flex-container-row">
+      	<div class="product-media">
+        <img class="product-image" src="obf/products/1EZ-Clean-Cart-w-deer-005-300x225.jpg" alt="EZ Clean Cart w/deer" />
+    	</div>
+        <div class="product-description">
+          <h3>E-Z CLEAN CART</h3>
+          <p>Cleaning a deer doesn’t have to be a back-breaking job!</p>
+
+          <p>$575</p>
+        </div>
+      </div>
+    </div>
+    
+    <br>
+    <br> 
+    
+    <div class="product">
+      <div class="product-row flex-container-row">
+       <div class="product-media">
+        <img class="product-image" src="obf/products/MICROWAVE-GRILL_09232013-300X240.png" alt="Microwave Grill" />
+    	</div>
+        <div class="product-description">
+          <h3>MICRO-WAVE GRILL</h3>
+          <p>Faster & hotter, with adjustable heat, thermometer included</p>
+          <p>Built to cook reliably for decades</p>
+          <p>$1200</p>
+        </div>
+      </div>
+    </div>
+    
+    <br>
+    <br>
+
+    <div class="product">
+      <div class="product-row flex-container-row">
+      	<div class="product-media">
+        <img class="product-image" src="obf/products/All_grtballoffire_20140929-300x150.png" alt="All great balls of fire" />
+        </div>
+        <div class="product-description">
+          <h3>GREAT BALL OF FIRE WILDLIFE SCENE EMBELLISHED FIRE PIT</h3>
+          <p>No two alike, each one a work of art!</p>
+          <p>1400 degree high-temperature powder-coat finish</p>
+          <p>Fire poker & ash can included</p>
+          <p>$2150</p>
+        </div>
+      </div>
+    </div>
+    
+    <br>
+    <br>
+
+  
+    </div>
+</div>
+  </main>
+<?php require_once dirname(__FILE__) . "/inc/footer.php"; ?>

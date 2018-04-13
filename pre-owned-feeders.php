d684b76e8f1d029e23c4a8232386f8c65725177b
webstandardcss <webstandardcss@gmail.com>
2018-04-13 15:24:31 -0500

    Created and finalized Specialty Products & Pre-owned feeders

---
+++ pre-owned-feeders.php
@@ -0,0 +1,43 @@
+<?php require_once dirname(__FILE__) . "/inc/config.php"; ?>
+
+<?php // Page data
+$pageTitle = "Homepage";
+$pageTitleTag = $siteName . " " . $pageTitle;
+$pageDescription = $siteName  . " " . $pageTitle;
+$pageKeywords = $pageTitle . "" . $siteKeywords;
+?>
+
+<?php require_once dirname(__FILE__) . "/inc/header.php"; ?>
+  <main class="ob-main flex-container">
+    <h1>Pre Owned Feeders</h1>
+    <article class="ob-content">
+      <section class="ob-section">
+        <p>These used feeders are ones that customers have traded back in on different models. All used feeders will be refurbished and repainted. Any electronics will be replaced with new components.</p>
+        
+        <br>
+        <br>
+
+        <p>Protein Feeders </p>
+
+        <p>1 BP 2000# – $1700</p>
+
+         <br>
+         
+        <p>Corn Feeders</p>
+
+         <br>
+
+        <p>Quail Feeders</p>
+
+        <p>2 Quail Central 600# – $1000 Each</p>
+
+        <p>2 Quail Central 1000# – $1050 Each</p>
+
+<br>
+<br>
+
+        <p>updated 2-6-2018</p>
+      </section>
+
+  </main>
+<?php require_once dirname(__FILE__) . "/inc/footer.php"; ?>
---
+++ specialty-products.php
@@ -0,0 +1,82 @@
+<?php require_once dirname(__FILE__) . "/inc/config.php"; ?>
+
+<?php // Page data
+$pageTitle = "Specialty Products";
+$pageTitleTag = $siteName . "Traps and Pens" . $pageTitle;
+$pageDescription = $siteName  . "Outback Feeders Traps and Pens" . $pageTitle;
+$pageKeywords = $pageTitle . "Protein Feeders gravity flow regulator" . $siteKeywords;
+?>
+
+<?php require_once dirname(__FILE__) . "/inc/header.php"; ?>
+  <main class="ob-main flex-container">
+    <h1>Specialty Products</h1>
+<br>
+<br>
+
+
+<div class="product">
+        <div class="product-row flex-container-row">
+          <div class="product-media">
+             <img class="product-image" src="obf/products/alfalfa-237x300.jpg" alt="Alfalfa Feeder" hspace="30"/>
+          </div>
+          <div class="product-description">
+             <h3>HAY & ALFALFA FEEDERS</h3>
+	          <p>Weather-proof, Feeder with Tray Single Bale Capacity</p>
+
+	          <p>$950</p>
+          </div>
+        </div>
+      </div>
+
+<br>
+<br>
+    <div class="product">
+      <div class="product-row flex-container-row">
+      	<div class="product-media">
+        <img class="product-image" src="obf/products/OB-multi-bale-hay-feeder-300x257.jpg" alt="Outback Multi-Bale Hay Feeder" />
+        </div>
+        <div class="product-description">
+          <h3>HAY & ALFALFA FEEDERS</h3>
+          <p>Multi-Bale Capacity (4 square bales)</p>
+          <p>$1850</p>
+          
+        </div>
+      </div>
+    </div>
+<br>
+<br>
+<br>
+
+ <div class="product">
+      <div class="product-row flex-container-row">
+      	<div class="product-media">
+        <img class="product-image" src="obf/products/Fawn-Specialty-feeders-275x300.png" alt="Fawn Specialty Feeder" />
+        </div>
+        <div class="product-description">
+          <h3>Fawn Specialty Products</h3>
+          <p>Fawn Tubes can be added to any Outback Protein Feeder</p>
+          <p>2 Port – $225</p>
+          <p>4 Port – $250</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="product">
+      <div class="product-row flex-container-row">
+      	<div class="product-media">
+        <img class="product-image" src="obf/products/600-Fawn-fdr-300x298.jpg" alt="600# Fawn FDR" />
+        </div>
+        <div class="product-description">
+          <h1>FAWN FEEDER</h1>
+          <p>600# – $1750</p>
+          
+        </div>
+      </div>
+    </div>
+   
+   	<br>
+   	<br>
+	<br>
+
+ </main>
+<?php require_once dirname(__FILE__) . "/inc/footer.php"; ?>

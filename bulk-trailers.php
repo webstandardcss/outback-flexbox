c8cf3721dd4dcad51a790f1c5c3104c2dee2d3db
webstandardcss <webstandardcss@gmail.com>
2018-04-13 16:44:38 -0500

    created and finalized the Bulk Trailers page

---
+++ bulk-trailers.php
@@ -0,0 +1,91 @@
+<?php require_once dirname(__FILE__) . "/inc/config.php"; ?>
+
+<?php // Page data
+$pageTitle = "";
+$pageTitleTag = $siteName . "Outback Feeders" . $pageTitle;
+$pageDescription = $siteName  . "" . $pageTitle;
+$pageKeywords = $pageTitle . "" . $siteKeywords;
+// $extraCss = array("forms");
+?>
+
+<?php require_once dirname(__FILE__) . "/inc/header.php"; ?>
+  <main class="ob-main flex-container">
+    <h1>Bulk Trailers</h1>
+    <p>Outback’s blower system delivers 160# of corn per minute and 125# of protein per minute.<p>
+
+<div class="product">
+        <div class="product-row flex-container-row">
+          <div class="product-media">
+            <img class="product-image" src="obf/products/QUAILPAR-SenderaRdFdrNoBlwr_20140916-237x300.jpg" alt="QUAILPAR Sendera" />
+          </div>
+          <div class="product-description">
+            <h3>SIDEKICK BULK TRAILER</h3>
+            <p>Equipped with remote control “Eliminator”, road feeder & gravity tube<br>
+            3000# – $5250</p>
+          </div>
+        </div>
+      </div>
+ <br>   
+    <div class="product">
+        <div class="product-row flex-container-row">
+          <div class="product-media">
+            <img class="product-image" src="obf/products/Pardner-3M_20140916-206x300.jpg" alt="Pardner 3<" />
+          </div>
+          <div class="product-description">
+            <h3>OUTBACK “PARDNER”</h3>
+            <p>Bulk Trailer with Remote Controlled “Eliminator”, Road Feeder, Gravity Tube & Blower System. Complete with tubing, fittings, ladder and spare tire.</p>
+            <p>3000# – $6975  (shown in picture)</p>
+            <p>4000# – $9125</p>
+            <p>6000# – $10500</p>
+          </div>
+        </div>
+      </div>
+<br>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/FJ6Bv4cg8GI" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+<br>
+ <div class="product">
+        <div class="product-row flex-container-row">
+          <div class="product-media">
+            <img class="product-image" src="obf/products/Pardner-6K-300x215.jpg" alt="    PARDNER 6K " />
+          </div>
+          <div class="product-description">
+            <h3>Double Barrel “PARDNER”</h3>
+            <p>Twin Bulk Trailer with Remote Controlled “Eliminator”, Road Feeder, Gravity Tube & Blower System.<br>
+            Complete with tubing, fittings, ladder & spare tire.</p>
+            <p>6000# – $10500</p>
+            
+          </div>
+        </div>
+      </div>
+
+  <br>
+  <br>
+
+    <div class="product">
+        <div class="product-row flex-container-row">
+          <div class="product-media">
+            <img class="product-image" src="obf/products/1quail-tender-225x300.jpg" alt="Quail Tender Trailer" />
+          </div>
+          <div class="product-description">
+            <h3>Quail Tender Trailer</h3>
+            <p>Features Outback “Pistolero” Directional Spinner Unit positioned to scatter feed out the side into the brush from the sendero while driving.<br>
+            Complete with tubing, fittings, ladder & spare tire.</p>
+            <p> 1000# – $4925</p>
+            <p>2000# – $5125</p>
+            <p>3000# – $5375</p>  
+            
+          </div>
+        </div>
+      </div>
+
+    <br>
+    <br> 
+    
+   
+
+    
+
+    
+    
+  </main>
+<?php require_once dirname(__FILE__) . "/inc/footer.php"; ?>

2ca6365d4681569a68262f020179ca3ea99d8d9c
webstandardcss <webstandardcss@gmail.com>
2018-04-12 22:05:14 -0500

    reformat blank.php

--- index.php
+++ index.php
@@ -11,10 +11,10 @@ $pageKeywords = $pageTitle . "" . $siteKeywords;
   <main class="ob-main flex-container">
     <h1>Welcome to Outback!</h1>
     <p>It all began in 1981 with Rick Meritt building custom steel fences and buildings as 3-M Steel Construction. Named 3-M (three Meritts) to include his wife and son.</p>
-	<p>As he built all over Texas, Rick developed products that better suited his first love – Hunting. Hence, Outback Wildlife Feeders was created and began with a single deer feeder. The product line has expanded to include about four dozen hunting, fishing and ranch equipment items.</p>
-	<p>Outback products are “Built Right the First Time, to Last a Lifetime”, withstanding extreme weather, wind and wildlife abuse.</p>
-	<p>All Outback products are innovatively designed and built at the original location in Gilmer, Texas. Made with only high quality American steel and craftsmanship, all products are 100% Satisfaction Guaranteed.</p>
-	<div class="product">
+	  <p>As he built all over Texas, Rick developed products that better suited his first love – Hunting. Hence, Outback Wildlife Feeders was created and began with a single deer feeder. The product line has expanded to include about four dozen hunting, fishing and ranch equipment items.</p>
+  	<p>Outback products are “Built Right the First Time, to Last a Lifetime”, withstanding extreme weather, wind and wildlife abuse.</p>
+  	<p>All Outback products are innovatively designed and built at the original location in Gilmer, Texas. Made with only high quality American steel and craftsmanship, all products are 100% Satisfaction Guaranteed.</p>
+	  <div class="product">
       <div class="product-row flex-container">
         <img class="product-image" src="obf/..." alt="" />
         <div class="product-description">

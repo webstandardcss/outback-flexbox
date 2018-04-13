af41f627414a3651de6ec65e67af20f7dc86bf5a
webstandardcss <webstandardcss@gmail.com>
2018-04-13 15:58:52 -0500

    created photo gallery page, it is not finished

---
+++ trophy-gallery.php
@@ -0,0 +1,86 @@
+<?php require_once dirname(__FILE__) . "/inc/config.php"; ?>
+
+<?php // Page data
+$pageTitle = "";
+$pageTitleTag = $siteName . "Outback Feeders" . $pageTitle;
+$pageDescription = $siteName  . "" . $pageTitle;
+$pageKeywords = $pageTitle . "" . $siteKeywords;
+// $extraCss = array("forms");
+?>
+
+<?php require_once dirname(__FILE__) . "/inc/header.php"; ?>
+  <main class="ob-main flex-container">
+    <h1>Trophy Gallery</h1>
+    
+	  <div class="product-small">
+        <h2>2016 Trophies</h2>
+        <div class="product-row flex-container-row">
+          <div class="product-single">
+            <div class="product-media">
+              <img class="product-image" src="obf/photo-gallery/IMG_3279-e1489604453150-1024x726.png" alt="New TX all-time #1 non Typical Whitetail. 311 5/8 gross. 304 6/8 net. 46 scorable pts 40" width="341" height="242" />
+            </div>
+            <div class="product-description">
+              <p>New TX all-time #1 non Typical Whitetail. 311 5/8" gross. 304 6/8" net. 46 scorable pts 40</p>
+            </div>
+          </div>
+          <div class="product-single">
+            <div class="product-media">
+              <img class="product-image" src="obf/products/" alt="Photo" />
+            </div>
+            <div class="product-description">
+              <p></p>
+            </div>
+          </div>
+          <div class="product-single">
+            <div class="product-media">
+              <img class="product-image" src="obf/products/" alt="Photo" />
+            </div>
+            <div class="product-description">
+              <p></p>
+            </div>
+          </div>
+          <div class="product-single">
+            <div class="product-media">
+              <img class="product-image" src="obf/products/" alt="Photo" />
+            </div>
+            <div class="product-description">
+              <p></p>
+            </div>
+          </div>
+          <div class="product-single">
+            <div class="product-media">
+              <img class="product-image" src="obf/products/" alt="Photo" />
+            </div>
+            <div class="product-description">
+             <p></p>
+            </div>
+          </div>
+          <div class="product-single">
+            <div class="product-media">
+              <img class="product-image" src="obf/products/" alt="Photo" />
+            </div>
+            <div class="product-description">
+              <p></p>
+            </div>
+          </div>
+          <div class="product-single">
+            <div class="product-media">
+              <img class="product-image" src="obf/products/" alt="Photo" />
+            </div>
+            <div class="product-description">
+             <p></p>
+            </div>
+          </div>
+          <div class="product-single">
+            <div class="product-media">
+              <img class="product-image" src="obf/products/" alt="Photo" />
+            </div>
+            <div class="product-description">
+              <p></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    
+  </main>
+<?php require_once dirname(__FILE__) . "/inc/footer.php"; ?>

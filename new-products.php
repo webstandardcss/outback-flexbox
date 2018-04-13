8c5b110cc6cba4d65234ba07d3d43658070a797d
webstandardcss <webstandardcss@gmail.com>
2018-04-13 16:22:37 -0500

    created and finalized the Road Doc page

---
+++ new-products.php
@@ -0,0 +1,56 @@
+<?php require_once dirname(__FILE__) . "/inc/config.php"; ?>
+
+<?php // Page data
+$pageTitle = "";
+$pageTitleTag = $siteName . "Outback Feeders" . $pageTitle;
+$pageDescription = $siteName  . "" . $pageTitle;
+$pageKeywords = $pageTitle . "" . $siteKeywords;
+// $extraCss = array("forms");
+?>
+
+<?php require_once dirname(__FILE__) . "/inc/header.php"; ?>
+  <main class="ob-main flex-container">
+    <h1>THE ROAD DOC</h1>
+    <p>Keeping your herd healthy is key to growing trophy animals. The Road Doc allows you to maintain the health of your deer, exotics and even quail by treating their feed or grain. The Road Doc is a dual purpose 200# road feeder that not only allows you to use it as a road feeder but also allows you to apply a liquid continuously and uniformly to your feed or grain.</p>
+
+    <p>Depending on the consistency of the liquid, it applies approximately 6.5 ml of liquid per pound of corn or 4 ml of liquid per pound of milo on demand by a handheld wired remote.
+    The liquids can be: prebiotics, probiotics, trace minerals, vitamins, medications, etc.</p>
+	  
+    <p>$1100</p>
+
+<br>
+<br>
+    <div class="product-half">
+       
+        <div class="product-row flex-container-row">
+          <div class="product-single">
+            <div class="product-media">
+              <img class="product-image" src="obf/products/IMG_5870-259x300.png" alt="IMG 5870" />
+            </div>
+            <div class="product-description">
+              
+            </div>
+          </div>
+          
+          <div class="product-single">
+            <div class="product-media">
+              <img class="product-image" src="obf/products/IMG_9807-300x243.jpg" alt="La Bandera" />
+            </div>
+            <div class="product-description">
+             
+            </div>
+          </div>
+          
+        </div>
+      </div>
+      <br>
+    <p>The reservoir holds 48 fl oz / 1420 ml.<br>
+    The spray pump and the feeder motor work simultaneously with the hand-held wired remote.
+    If you are not wanting to apply a liquid to the feed or grain simply turn off the toggle switch located by the reservoir.</p>
+    <br>
+    <br>
+    <iframe width="940" height="529" src="https://www.youtube.com/embed/clZL193meXQ" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+    <br>
+    <br>
+  </main>
+<?php require_once dirname(__FILE__) . "/inc/footer.php"; ?>

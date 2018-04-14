ffe3be5b005c8c4e10954bd84e00a11b601e95b5
webstandardcss <webstandardcss@gmail.com>
2018-04-14 04:47:20 -0500

    get mail form working on homepage

--- index.php
+++ index.php
@@ -42,37 +42,50 @@ iframe {
       <h1>Get In Touch</h1>
 
       <div class="gform_wrapper" id="gform_wrapper_4">
-        <form method="post" enctype="multipart/form-data" id="gform_4" action="">
+        <form method="post" enctype="multipart/form-data" id="gform_4" action="http://new.outbackfeeders.com/obf/mail/mail.php">
+          <input type="hidden" name="recipients" value="myaddress" />
+          <input type="hidden" name="good_url" value="/" />
+          <input type="hidden" name="subject" value="Outback Homepage Get In Touch Form" />
+          <input type="hidden" name="csvfile" value="mail-homepage.csv" />
+          <input type="hidden" name="logfile" value="mail-homepage.log" />
+          <input type="hidden" name="csvcolumns"
+              value="email:c,realname:c,phone:cs,zipcode:cs,product:r" />
+          <input type="hidden" name="required"
+              value="email:Your email address,
+              realname:Your name,
+              phone:Your phone number,
+              zipcode:Your zip code,
+              product:The reason you're interested in our product" />
           <div class="gform_body">
             <ul id="gform_fields_4" class="gform_fields top_label form_sublabel_below description_below">
               <li id="field_4_1" class="gfield gfield_contains_required field_sublabel_below field_description_below gfield_visibility_visible">
-                <label class="gfield_label" for="input_4_1">Name<span class="gfield_required">*</span></label>
+                <label class="gfield_label" for="realname">Name<span class="gfield_required">*</span></label>
                 <div class="ginput_container ginput_container_text">
-                  <input name="input_1" id="input_4_1" type="text" value="" class="medium" tabindex="1" placeholder="Name" aria-required="true" aria-invalid="false">
+                  <input name="realname" id="realname" type="text" value="" class="medium" tabindex="1" placeholder="Name" aria-required="true" aria-invalid="false">
                 </div>
               </li>
               <li id="field_4_3" class="gfield gfield_contains_required field_sublabel_below field_description_below gfield_visibility_visible">
-                <label class="gfield_label" for="input_4_3">Phone<span class="gfield_required">*</span></label>
+                <label class="gfield_label" for="phone">Phone<span class="gfield_required">*</span></label>
                 <div class="ginput_container ginput_container_text">
-                  <input name="input_3" id="input_4_3" type="text" value="" class="medium" tabindex="3" placeholder="Phone" aria-required="true" aria-invalid="false">
+                  <input name="phone" id="phone" type="text" value="" class="medium" tabindex="3" placeholder="Phone" aria-required="true" aria-invalid="false">
                 </div>
               </li>
               <li id="field_4_8" class="gfield dealer-field-right gfield_contains_required field_sublabel_below field_description_below gfield_visibility_visible">
-                <label class="gfield_label" for="input_4_8">Email<span class="gfield_required">*</span></label>
+                <label class="gfield_label" for="email">Email<span class="gfield_required">*</span></label>
                 <div class="ginput_container ginput_container_email">
-                  <input name="input_8" id="input_4_8" type="text" value="" class="medium" tabindex="4" placeholder="Email" aria-required="true" aria-invalid="false">
+                  <input name="email" id="email" type="text" value="" class="medium" tabindex="4" placeholder="Email" aria-required="true" aria-invalid="false">
                 </div>
               </li>
               <li id="field_4_6" class="gfield field_sublabel_below field_description_below gfield_visibility_visible">
-                <label class="gfield_label" for="input_4_6">Your Zipcode</label>
+                <label class="gfield_label" for="zipcode">Your Zipcode</label>
                 <div class="ginput_container ginput_container_text">
-                  <input name="input_6" id="input_4_6" type="text" value="" class="medium" tabindex="5" placeholder="Your Zipcode" aria-invalid="false">
+                  <input name="zipcode" id="zipcode" type="text" value="" class="medium" tabindex="5" placeholder="Your Zipcode" aria-invalid="false">
                 </div>
               </li>
               <li id="field_4_4" class="gfield dealer-field-right gfield_contains_required field_sublabel_below field_description_below gfield_visibility_visible">
-                <label class="gfield_label" for="input_4_4">Products You Are Interested In<span class="gfield_required">*</span></label>
+                <label class="gfield_label" for="product">Products You Are Interested In<span class="gfield_required">*</span></label>
                 <div class="ginput_container ginput_container_select">
-                  <select name="input_4" id="input_4_4" class="medium gfield_select" tabindex="6" aria-required="true" aria-invalid="false">
+                  <select name="product" id="product" class="medium gfield_select" tabindex="6" aria-required="true" aria-invalid="false">
                     <option value="" selected="selected" class="gf_placeholder">Products You Are Interested In</option>
                     <option value="Corn Feeders">Corn Feeders</option>
                     <option value="Protein Feeders">Protein Feeders</option>
@@ -81,9 +94,10 @@ iframe {
                 </div>
               </li>
               <li id="field_4_7" class="gfield message-text field_sublabel_below field_description_below gfield_visibility_visible">
-                <label class="gfield_label" for="input_4_7">Comments</label>
-                <div class="ginput_container ginput_container_textarea">
-                  <textarea name="input_7" id="input_4_7" class="textarea medium" tabindex="7" placeholder="Comments" aria-invalid="false" rows="10" cols="50"></textarea>
+                <label class="gfield_label" for="comments">Comments</label>
+                <div class="ginput_container">
+                  <img src="http://new.outbackfeeders.com/obf/mail/verifyimg.php" alt="Image verification" name="vimg" />
+                  <input type="text" size="12" name="imgverify" />
                 </div>
               </li>
             </ul>

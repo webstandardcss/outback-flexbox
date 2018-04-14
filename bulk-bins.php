cfbe7d13559612236396403bd2cf7850f15a43f8
webstandardcss <webstandardcss@gmail.com>
2018-04-14 10:13:21 -0500

    creted and finalized bulk hoppers and bins pages

---
+++ bulk-bins.php
@@ -0,0 +1,32 @@
+<?php require_once dirname(__FILE__) . "/inc/config.php"; ?>
+
+<?php // Page data
+$pageTitle = "";
+$pageTitleTag = $siteName . "Outback Feeders" . $pageTitle;
+$pageDescription = $siteName  . "" . $pageTitle;
+$pageKeywords = $pageTitle . "" . $siteKeywords;
+// $extraCss = array("forms");
+?>
+
+<?php require_once dirname(__FILE__) . "/inc/header.php"; ?>
+  <main class="ob-main flex-container">
+    <h1>Bulk Bins</h1>
+    
+	  <div class="product">
+      <div class="product-row flex-container-row">
+        <div class="product-media">
+        <img class="product-image" src="obf/products/NEWFEEB-Poco-Blk-bin-768x1024.jpg" alt="Poco Blk Bin" width="384" height="512" />
+      </div>
+        <div class="product-description">
+          <h3>POCO BULK BIN</h3>
+          <p>1000# – $1950<br>
+          2000# – $2050<br>
+          3000# – $2350</p>
+        </div>
+      </div>
+    </div>
+    
+ 
+
+  </main>
+<?php require_once dirname(__FILE__) . "/inc/footer.php"; ?>
---
+++ bulk-hoppers.php
@@ -0,0 +1,47 @@
+<?php require_once dirname(__FILE__) . "/inc/config.php"; ?>
+
+<?php // Page data
+$pageTitle = "";
+$pageTitleTag = $siteName . "Outback Feeders" . $pageTitle;
+$pageDescription = $siteName  . "" . $pageTitle;
+$pageKeywords = $pageTitle . "" . $siteKeywords;
+// $extraCss = array("forms");
+?>
+
+<?php require_once dirname(__FILE__) . "/inc/header.php"; ?>
+  <main class="ob-main flex-container">
+    <h1>Bulk Hoppers</h1>
+    
+	    <div class="product">
+        <h2>Regular Product Layout</h2>
+        <div class="product-row flex-container-row">
+          <div class="product-media">
+            <img class="product-image" src="obf/products/1NEWFEE2.-500-trk-hopper-300x261.jpg" alt="Truck Hopper" />
+          </div>
+          <div class="product-description">
+            <h3>TRUCK & TRAILER HOPPERS</h3>
+            <p>With Remote Controlled “Eliminator”, Road Feeder, Gravity Tube & Blower System</p>
+
+            <p>600# –   $3780  (shown left)<br>
+            1000# – $3900<br>
+            2000# – $4250<br>
+            3000# – $4550</p>
+          </div>
+        </div>
+      </div>
+      <br>
+        <div class="product">
+
+        <div class="product-row flex-container-row">
+          <div class="product-media">
+            <img class="product-image" src="obf/products/NEWFEE6-4K-trlr-hpr-300x225.jpg" alt="Trailer Hopper" />
+          </div>
+          <div class="product-description">
+            
+            <p>4000# – $5575</p>
+          </div>
+        </div>
+      </div>
+    
+  </main>
+<?php require_once dirname(__FILE__) . "/inc/footer.php"; ?>

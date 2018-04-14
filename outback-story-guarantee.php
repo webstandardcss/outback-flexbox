a0f9be9f359cdc45ea3451ccc6a51efc3c08769f
webstandardcss <webstandardcss@gmail.com>
2018-04-14 00:09:45 -0500

    fix guarantee large images for Luis

--- outback-story-guarantee.php
+++ outback-story-guarantee.php
@@ -7,6 +7,15 @@ $pageDescription = $siteName  . " " . $pageTitle;
 $pageKeywords = $pageTitle . "" . $siteKeywords;
 ?>
 
+<style>
+.product-image {
+  width: 100%;
+  min-width: 100%;
+  height: auto;
+  display: block;
+}
+</style>
+
 <?php require_once dirname(__FILE__) . "/inc/header.php"; ?>
   <main class="ob-main flex-container">
     <h1>Welcome to Outback!</h1>
@@ -14,13 +23,7 @@ $pageKeywords = $pageTitle . "" . $siteKeywords;
 	<p>As he built all over Texas, Rick developed products that better suited his first love – Hunting. Hence, Outback Wildlife Feeders was created and began with a single deer feeder. The product line has expanded to include about four dozen hunting, fishing and ranch equipment items.</p>
 	<p>Outback products are “Built Right the First Time, to Last a Lifetime”, withstanding extreme weather, wind and wildlife abuse.</p>
 	<p>All Outback products are innovatively designed and built at the original location in Gilmer, Texas. Made with only high quality American steel and craftsmanship, all products are 100% Satisfaction Guaranteed.</p>
-    <div class="product">
-      <div class="product-row flex-container">
-        <img class="product-image" src="obf/Hunting Photos/new14-141.jpg" alt="Rick Merit with deer" />
-        <div class="product-description">
-        </div>
-      </div>
-    </div>
+  <img class="product-image" src="obf/Hunting Photos/new14-141.jpg" alt="Rick Merit with deer" />v>
     <h1>Lifetime Warranty</h1>
 	<p>10 year manufacturing defect. We will personally repair, replace or hire a competent repair team to come to your location. After 10 years we will repair at our location in Gilmer Texas. 100% Satisfaction</p>
 	<p>Numerous patents are held by Outback and include: 8201520, 7370605, 6920841, D575908, D599503, D602649, D603104, D603105, D604017, D603566, D624706, D624708, D622453, D624707, D636942, D629572, D629976, D629975, D624709, D630802, D630653, D647253, D642750, D681883. Additional patents are pending. U.S. trademarks included 3629190 & 3625066.</p>

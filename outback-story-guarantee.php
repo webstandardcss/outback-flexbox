d78e67ba727ea4a156b0fe40e124ec38fb3d33bc
webstandardcss <webstandardcss@gmail.com>
2018-04-14 01:26:49 -0500

    Luis please rename Hunting Photos to hunting-photos

--- outback-story-guarantee.php
+++ outback-story-guarantee.php
@@ -20,14 +20,14 @@ $pageKeywords = $pageTitle . "" . $siteKeywords;
   <main class="ob-main flex-container">
     <h1>Welcome to Outback!</h1>
     <p>It all began in 1981 with Rick Meritt building custom steel fences and buildings as 3-M Steel Construction. Named 3-M (three Meritts) to include his wife and son.</p>
-	<p>As he built all over Texas, Rick developed products that better suited his first love – Hunting. Hence, Outback Wildlife Feeders was created and began with a single deer feeder. The product line has expanded to include about four dozen hunting, fishing and ranch equipment items.</p>
-	<p>Outback products are “Built Right the First Time, to Last a Lifetime”, withstanding extreme weather, wind and wildlife abuse.</p>
-	<p>All Outback products are innovatively designed and built at the original location in Gilmer, Texas. Made with only high quality American steel and craftsmanship, all products are 100% Satisfaction Guaranteed.</p>
-  <img class="product-image" src="obf/Hunting Photos/new14-141.jpg" alt="Rick Merit with deer" />v>
+    <p>As he built all over Texas, Rick developed products that better suited his first love – Hunting. Hence, Outback Wildlife Feeders was created and began with a single deer feeder. The product line has expanded to include about four dozen hunting, fishing and ranch equipment items.</p>
+    <p>Outback products are “Built Right the First Time, to Last a Lifetime”, withstanding extreme weather, wind and wildlife abuse.</p>
+    <p>All Outback products are innovatively designed and built at the original location in Gilmer, Texas. Made with only high quality American steel and craftsmanship, all products are 100% Satisfaction Guaranteed.</p>
+    <img class="product-image" src="obf/hunting-photos/new14-141.jpg" alt="Rick Merit with deer" />
     <h1>Lifetime Warranty</h1>
-	<p>10 year manufacturing defect. We will personally repair, replace or hire a competent repair team to come to your location. After 10 years we will repair at our location in Gilmer Texas. 100% Satisfaction</p>
-	<p>Numerous patents are held by Outback and include: 8201520, 7370605, 6920841, D575908, D599503, D602649, D603104, D603105, D604017, D603566, D624706, D624708, D622453, D624707, D636942, D629572, D629976, D629975, D624709, D630802, D630653, D647253, D642750, D681883. Additional patents are pending. U.S. trademarks included 3629190 & 3625066.</p>
-	<img class="product-image" src="obf/Misc/OB-entrance-at-sunset_20140918-1024x768.jpg" alt="Sunset at the Merit ranch" />
+    <p>10 year manufacturing defect. We will personally repair, replace or hire a competent repair team to come to your location. After 10 years we will repair at our location in Gilmer Texas. 100% Satisfaction</p>
+    <p>Numerous patents are held by Outback and include: 8201520, 7370605, 6920841, D575908, D599503, D602649, D603104, D603105, D604017, D603566, D624706, D624708, D622453, D624707, D636942, D629572, D629976, D629975, D624709, D630802, D630653, D647253, D642750, D681883. Additional patents are pending. U.S. trademarks included 3629190 & 3625066.</p>
+    <img class="product-image" src="obf/Misc/OB-entrance-at-sunset_20140918-1024x768.jpg" alt="Sunset at the Merit ranch" />
     </div>
   </main>
 <?php require_once dirname(__FILE__) . "/inc/footer.php"; ?>

21a53acbe5906cd34185e47f2633c99b697f7fc2
webstandardcss <webstandardcss@gmail.com>
2018-04-16 01:28:01 -0500

    try to fix search engine

---
+++ search.php
@@ -0,0 +1,8 @@
+<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400">
+<link rel="stylesheet" href="/assets/vendors/tipuesearch/css/normalize.css">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<link rel="stylesheet" href="/assets/vendors/tipuesearch/css/tipuesearch.css">
+<script src="/assets/vendors/tipuesearch/tipuesearch_set.js"></script>
+<script src="/assets/vendors/tipuesearch/tipuesearch.min.js"></script>

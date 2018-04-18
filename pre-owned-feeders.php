c13cefc340040e32b8322f891a73083d0f7e62da
webstandardcss <webstandardcss@gmail.com>
2018-04-18 00:33:41 -0500

    add classes to the pre-owned feeders page so we can style it later

--- pre-owned-feeders.php
+++ pre-owned-feeders.php
@@ -12,31 +12,31 @@ $pageKeywords = $pageTitle . "  " . $siteKeywords;
     <h1>Pre Owned Feeders</h1>
     <article class="ob-content">
       <section class="ob-section">
-        <p>These used feeders are ones that customers have traded back in on different models. All used feeders will be refurbished and repainted. Any electronics will be replaced with new components.</p>
+        <p class="section-description">These used feeders are ones that customers have traded back in on different models. All used feeders will be refurbished and repainted. Any electronics will be replaced with new components.</p>
         
         <br>
         <br>
 
-        <p>Protein Feeders </p>
+        <h4 class="align-left">Protein Feeders </h4>
 
-        <p>1 BP 2000# – $1700</p>
+        <p class="product-line">1 BP 2000# – <span class="product-price">$1700</span></p>
 
          <br>
          
-        <p>Corn Feeders</p>
+        <h4 class="align-left">Corn Feeders</h4>
 
          <br>
 
-        <p>Quail Feeders</p>
+        <h4 class="align-left">Quail Feeders</h4>
 
-        <p>2 Quail Central 600# – $1000 Each</p>
+        <p class="product-line">2 Quail Central 600# – <span class="product-price">$1000</span> Each</p>
 
-        <p>2 Quail Central 1000# – $1050 Each</p>
+        <p class="product-line">2 Quail Central 1000# – <span class="product-price">$1050</span> Each</p>
 
-<br>
-<br>
+        <br>
+        <br>
 
-        <p>updated 2-6-2018</p>
+        <small>updated 2-6-2018</small>
       </section>
 
   </main>

e35546aff858638b0db5e6927a7b20ac9bcf151d
webstandardcss <webstandardcss@gmail.com>
2018-04-22 15:55:42 -0500

    Fix mobile nav correct URLs

--- inc/_mobilenav.php
+++ inc/_mobilenav.php
@@ -1,38 +1,38 @@
 <select id="menu-main-1" class="navselect">
   <option selected="selected" disabled="disabled">Menu</option>
   <option value="/">Home</option>
-  <option value="./outback-story-guarantee.php">About Us
+  <option value="/outback-story-guarantee.php">About Us
   </option>
-  <option value="./contact-us.php">–&nbsp;Contact Us</option>
-  <option value="./delivery-information.php">–&nbsp;Delivery Information</option>
-  <option value="./outback-story-guarantee.php">–&nbsp;Outback Story &amp; Guarantee</option>
-  <option value="http://outbackfeeders.com/why-you-should-buy-outback-feeders/">Why you should buy Outback Feeders</option>
-  <option value="http://outbackfeeders.com/corn-feeders/">Corn Feeders
+  <option value="/contact-us.php">–&nbsp;Contact Us</option>
+  <option value="/delivery-information.php">–&nbsp;Delivery Information</option>
+  <option value="/outback-story-guarantee.php">–&nbsp;Outback Story &amp; Guarantee</option>
+  <option value="/why-you-should-buy-outback-feeders.php">Why you should buy Outback Feeders</option>
+  <option value="/corn-feeders.php">Corn Feeders
   </option>
-  <option value="http://outbackfeeders.com/baby-back-series/">–&nbsp;BABY-BACK Low Profile Feeders</option>
-  <option value="http://outbackfeeders.com/top-gun-corn-feeder/">–&nbsp;Top Gun Corn Feeder</option>
-  <option value="http://outbackfeeders.com/protein-feeders/">Protein Feeders
+  <option value="/baby-back-feeder-series.php">–&nbsp;BABY-BACK Low Profile Feeders</option>
+  <option value="/top-gun-corn-feeder.php">–&nbsp;Top Gun Corn Feeder</option>
+  <option value="/protein-feeders.php">Protein Feeders
   </option>
-  <option value="http://outbackfeeders.com/half-back-protein/">–&nbsp;Half-Back Protein Feeder</option>
-  <option value="http://outbackfeeders.com/the-doc/">–&nbsp;The Doc</option>
-  <option value="http://outbackfeeders.com/the-coontroller/">–&nbsp;THE COONTROLLER</option>
-  <option value="http://outbackfeeders.com/protein-corn-combo-feeders/">Protein &amp; Corn Combo Feeders</option>
-  <option value="http://outbackfeeders.com/fish-feeder-share-kropper/">Fish Feeder</option>
-  <option value="http://outbackfeeders.com/quail-turkey-feeders/">Quail &amp; Turkey Feeders</option>
-  <option value="http://outbackfeeders.com/water-stations/">Water Stations</option>
-  <option value="http://outbackfeeders.com/feed-delivery-systems/">Feed Delivery Systems
+  <option value="/half-back-protein.php">–&nbsp;Half-Back Protein Feeder</option>
+  <option value="/the-doc.php">–&nbsp;The Doc</option>
+  <option value="/the-coontroller.php">–&nbsp;THE COONTROLLER</option>
+  <option value="/protein-corn-combo-feeders.php">Protein &amp; Corn Combo Feeders</option>
+  <option value="/fish-feeder-share-kropper.php">Fish Feeder</option>
+  <option value="/quail-turkey-feeders.php">Quail &amp; Turkey Feeders</option>
+  <option value="/water-stations.php">Water Stations</option>
+  <option value="/feed-delivery-systems.php">Feed Delivery Systems
   </option>
-  <option value="http://outbackfeeders.com/new-products/">–&nbsp;THE ROAD DOC</option>
-  <option value="http://outbackfeeders.com/bulk-feed-systems/bulk-trailers/">–&nbsp;Bulk Trailers</option>
-  <option value="http://outbackfeeders.com/bulk-feed-systems/bulk-hoppers/">–&nbsp;Bulk Hoppers</option>
-  <option value="http://outbackfeeders.com/bulk-feed-systems/bulk-bins/">–&nbsp;Bulk Bins</option>
-  <option value="http://outbackfeeders.com/traps-pens/">Traps &amp; Pens</option>
-  <option value="http://outbackfeeders.com/hunt-camp-equipment/">Hunt &amp; Camp Equipment</option>
-  <option value="http://outbackfeeders.com/specialty-products/">Specialty Products</option>
-  <option value="http://outbackfeeders.com/pre-owned-feeders/">Pre Owned Feeders</option>
-  <option value="http://outbackfeeders.com/trophy-gallery/">Trophy Gallery</option>
-  <option value="http://outbackfeeders.com/become-a-dealer/">Become a Dealer</option>
-  <option value="http://outbackfeeders.com/about-us/outback-dealers/">Outback Dealer List</option>
+  <option value="/new-products.php">–&nbsp;THE ROAD DOC</option>
+  <option value="/bulk-trailers.php">–&nbsp;Bulk Trailers</option>
+  <option value="/bulk-hoppers.php">–&nbsp;Bulk Hoppers</option>
+  <option value="/bulk-bins.php">–&nbsp;Bulk Bins</option>
+  <option value="/traps-pens.php">Traps &amp; Pens</option>
+  <option value="/hunt-camp-equipment.php">Hunt &amp; Camp Equipment</option>
+  <option value="/specialty-products.php">Specialty Products</option>
+  <option value="/pre-owned-feeders.php">Pre Owned Feeders</option>
+  <option value="/trophy-gallery.php">Trophy Gallery</option>
+  <option value="/become-a-dealer.php">Become a Dealer</option>
+  <option value="/outback-dealers.php">Outback Dealer List</option>
   <option value="http://KickinBackKreations.com">Kickin' Back Kreations</option>
 </select>
 <span class="mobilenav-button"><span class="headerfont">Menu </span><i class="fas fa-align-justify"></i></span>
